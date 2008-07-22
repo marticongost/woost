645bd6f302c2ddcb44a46ace876afc7f25e2acc9
Mart? Congost <marti.congost@whads.com>
2008-07-22 17:15:15 +0200

    Started implementing declarative queries

--- persistence/query.py
+++ persistence/query.py
@@ -6,9 +6,30 @@
 @organization:	Whads/Accent SL
 @since:			July 2008
 """
+from itertools import chain
 from magicbullet.modeling import getter
+from magicbullet.persistence import EntityClass
+from magicbullet.schema import expressions
 
 class Query(object):
+
+    _indexable_expressions = set([
+        expressions.EqualExpression,
+        expressions.NotEqualExpression,
+        expressions.GreaterExpression,
+        expressions.GreaterEqualExpression,
+        expressions.LowerExpression,
+        expressions.LowerEqualExpression
+    ])
+
+    _expression_speed = {
+        expressions.EqualExpression: -1,
+        expressions.StartsWithExpression: 1,
+        expressions.EndsWithExpression: 1,
+        expressions.ContainsExpression: 1,
+        expressions.MatchExpression: 2,
+        expressions.SearchExpression: 2
+    }
 
     def __init__(self,
         entity_class,
@@ -16,63 +37,259 @@ class Query(object):
         order = None,
         range = None):
 
+        if not isinstance(entity_class, EntityClass) \
+        or not entity_class.indexed:
+            raise TypeError("An indexed entity class is required")
+
         self.__entity_class = entity_class
+        self.__parent = None
+        self.__filters = filter if isinstance(filter, list) else [filter]
+        self.__order = order
+        self.__range = range
 
-    @classmethod
-    def parse(self, string):
-        pass
+    @getter
+    def entity_class(self):
+        return self.__entity_class
 
-    def serialize(self):
-        pass
+    @getter
+    def parent(self):
+        return self.__parent
+
+    def execute(self):
+        subset = self.__apply_filters()        
+        subset = self.__apply_order(subset)
+        subset = self.__apply_range(subset)        
+        return subset
+
+    def __apply_filters(self):
+
+        instances = self.__entity_class.index
+
+        filters = self.filters
+
+        if filters:
+            
+            # Create an optimized execution plan
+            execution_plan = []
+
+            for filter in filters:
+                member = filter.operands[0]                
+                expr_speed = self._expression_speed.get(filter.__class__, 0)
+
+                if member.indexed \
+                and filter.__class__ in self._indexable_expressions:
+                    if member.unique:
+                        indexing = 0
+                    else:
+                        indexing = 1
+                else:
+                    indexing = 2
+                
+                order = (indexing, expr_speed)
+                execution_plan.append((order, filter))
+
+            execution_plan.sort()
+            
+            # Apply the execution plan
+            subset = None
+            single_match = False
+
+            for order, filter in execution_plan:
+                
+                member, value = filter.operands
+
+                # Apply the filter using an index
+                if member.indexed and not single_match:
+
+                    # Equal
+                    if isinstance(filter, expressions.EqualExpression):
+
+                        if member.unique:
+                            match = member.index.get(value)
+                            
+                            if match:
+                                subset = set([match])
+                                
+                                # Special case: after an 'identity' filter is
+                                # resolved (an equality check against a unique
+                                # index), all further filters will ignore
+                                # indices and use direct logical matching
+                                # instead (should be faster)
+                                single_match = True
+                            else:
+                                subset = set()
+                        else:
+                            matches = member.index.get(value)
+                    
+                    # Different
+                    elif isinstance(filter, expressions.NotEqualExpression):
+                        lower = member.index.values(
+                            max = value, excludemax = True)
+                        higher = member.index.values(
+                            min = value, excludemin = True)
+                        matches = chain(lower, higher)
+
+                    # Greater
+                    elif isinstance(filter, (
+                        expressions.GreaterExpression,
+                        expressions.GreaterEqualExpression
+                    )):
+                        matches = member.index.values(
+                            min = value,
+                            excludemax = isinstance(filter,
+                                expressions.GreaterExpression)
+                        )
+                
+                    # Lower
+                    elif isinstance(filter, (
+                        expressions.LowerExpression,
+                        expressions.LowerEqualExpression
+                    )):
+                        matches = member.index.values(
+                            max = value,
+                            excludemax = isinstance(filter,
+                                expressions.LowerExpression)
+                        )
+
+                    # Add matches together
+                    if matches:
+                        if subset:
+                            if not isinstance(subset, set):
+                                subset = set(subset)
+
+                            subset = subset.intersection(matches)
+                        else:
+                            subset = matches
+
+                # Brute force matching
+                else:
+                    if subset is None:
+                        subset = instances.itervalues()
+                    
+                    subset = [instance
+                              for instance in subset
+                              if filter.eval(instance.__dict__)]
+
+                # As soon as the matching set is reduced to an empty set
+                # there's no point in applying any further filter
+                if not subset:
+                    break
+
+            instances = subset
+
+        return instances
+
+    def __apply_order(self, subset):
+        
+        order = self.order
+
+        if order is None:
+            order = [+self.__entity_class.id]
+
+        main_order = order[0]
+        main_criteria = main_order.operands[0].operands[0]
+        filters = self.filters
+
+        unaltered_order = not filters or (
+            len(filters) == 1 and filters[0].operands[0] is main_criteria
+        )
+
+        # Sort using an index
+        if main_criteria.indexed and len(order) == 1 and unaltered_order:
+
+            if not filters:
+
+                # Prepend instances that aren't present in the index (all
+                # instances with a `None` value)
+                if not main_criteria.required:
+                    none_instances = difference(
+                        self.__entity_class.index,
+                        main_criteria.index
+                    )
+                    subset = chain(none_instances, main_criteria.index)
+
+            if isinstance(main_order, expressions.NegativeExpression):
+                if not isinstance(subset, list):
+                    subset = list(subset)
+                subset.reverse()
+
+        # Regular sort
+        else:
+            if not isinstance(subset, list):
+                subset = list(subset)
+
+            cmp_sequence = [
+                (
+                    expr.operands[0],
+                    isinstance(expr, expressions.NegativeExpression)
+                )
+                for expr in order
+            ]
+
+            def compare(a, b):
+                
+                values_a = []
+                values_b = []
+
+                for member, reversed_member in cmp_sequence:
+                    value_a = a.get(member)
+                    value_b = b.get(member)
+
+                    if reversed_member:
+                        value_a, value_b = value_b, value_a
+
+                    values_a.append(value_a)
+                    values_b.append(value_b)
+
+                return cmp(values_a, values_b)
+
+            subset.sort(cmp = compare)
+
+        return subset
+
+    def __apply_range(self, subset):
+
+        range = self.range
+
+        if range:
+            subset = subset[range[0]:range[1]]
+
+        return subset
 
     def __iter__(self):
-        
-        # Filtering
-        filter = self.filter
-
-        if filter:
-            pass
-
-        # Order
-
-        # Range
+        return self.execute().__iter__()
 
     def __len__(self):
-        item_count = 0
-        for item in self:
-            item_count += 1
-        return item_count
+        return len(self.execute())
 
     def __notzero__(self):
-        try:
-            iter(self).next()
-        except StopIteration:
-            return False
-        else:
-            return True
+        return bool(self.execute())
 
     def __contains__(self, item):
-        for match in self:
-            if item is match:
-                return True
-        return False
+        return bool(self.select_by(id = item.id))
 
     def select(self,
         filter = None,
         order = None,
         range = None):
-        pass
+        
+        child_query = self.__class__(
+            self.__entity_class,
+            filter,
+            order,
+            range)
+
+        child_query.__parent = self
+        return child_query
 
     def select_by(self, **kwargs):
-        pass
+        for key, value in kwargs.itervalues():
+            member = getattr(self.__entity_class, key)
+            self.add_filter(key == value)
 
     def add_filter(self, filter):
         if self.__filter is None:
             self.__filter = filter
         else:
             self.__filter &= filter
-
-    @getter
-    def entity_class(self):
-        return self.__entity_class
 
