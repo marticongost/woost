7486e1114fba9df703d667b269f14efb4dc898fb
Mart? Congost <marti.congost@whads.com>
2008-11-03 11:47:26 +0100

    More refactoring to the item edit controller. Restored the previous behavior for the add/delete translation option.

---
+++ controllers/backoffice/itemsectioncontroller.py
@@ -0,0 +1,218 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			November 2008
+"""
+from __future__ import with_statement
+import cherrypy
+from cocktail.modeling import cached_getter
+from cocktail.schema import Adapter, Collection, String, ErrorList
+from sitebasis.models import Site
+from sitebasis.controllers.backoffice.basebackofficecontroller \
+        import BaseBackOfficeController
+
+
+class ItemSectionController(BaseBackOfficeController):
+
+    @cached_getter
+    def edit_state(self):
+        return self.parent.edit_state
+
+    @cached_getter
+    def item(self):
+        return self.parent.item
+
+    @cached_getter
+    def content_type(self):
+        return self.parent.content_type
+
+    @cached_getter
+    def form_adapter(self):
+        adapter = Adapter()
+        adapter.exclude(
+            ["id",
+             "author",
+             "owner",
+             "creation_time",
+             "last_update_time",
+             "drafts",
+             "draft_source",
+             "is_draft"]
+            + [member.name
+               for member in self.content_type.members().itervalues()
+               if isinstance(member, Collection)]
+        )
+        return adapter
+
+    @cached_getter
+    def form_schema(self):
+        form_schema = self.form_adapter.export_schema(self.content_type)
+        form_schema.name = "BackOfficeEditForm"
+        return form_schema
+
+    @cached_getter
+    def form_data(self):
+        
+        form_data = self.edit_state.form_data
+        
+        # Load model data into the form
+        if form_data is None:
+
+            form_data = {}
+
+            # Item data
+            form_source = self.item
+            source_schema = self.content_type
+
+            # Default data
+            if not form_source:
+                form_source = {}
+                source_schema.init_instance(form_source)
+
+            self.form_adapter.export_object(
+                form_source,
+                form_data,
+                source_schema,
+                self.form_schema
+            )
+
+            self.edit_state.form_data = form_data
+
+        return form_data
+
+    @cached_getter
+    def form_errors(self):
+        return ErrorList(
+            self.form_schema.get_errors(
+                self.form_data,
+                persistent_object = self.item
+            )
+        )
+
+    @cached_getter
+    def differences(self):
+
+        if self.item:
+            form_keys = set(self.form_schema.members().iterkeys())
+            return [
+                (member, language)
+                for member, language in self.content_type.differences(
+                    self.item.draft_source or self.item,
+                    self.form_data
+                )
+                if member.name in form_keys \
+                    and not isinstance(member, Collection)
+            ]
+        else:
+            return set()
+
+    @cached_getter
+    def available_languages(self):
+        return Site.main.languages
+
+    @cached_getter
+    def translations(self):
+
+        edit_state = self.edit_state
+
+        # Determine active translations
+        if edit_state.translations is None:
+            if self.item and self.item.__class__.translated:
+                edit_state.translations = self.item.translations.keys()
+            else:
+                edit_state.translations = list(self.get_visible_languages())
+        
+        return edit_state.translations
+
+    @cached_getter
+    def action(self):
+        return self.params.read(String("action"))
+
+    @cached_getter
+    def submitted(self):
+        return self.action in ("save", "make_draft")
+
+    def is_ready(self):
+        return self.submitted and not self.form_errors
+
+    def submit(self):
+
+        redirect = False
+        item = self.item
+        user = self.user
+
+        # Create a draft
+        if self.action == "make_draft":
+
+            # From an existing element
+            if item:
+                item = item.make_draft()
+
+            # From scratch
+            else:
+                item = self.content_type()
+                item.is_draft = True
+
+            item.author = user
+            item.owner = user
+            redirect = True
+
+        # Store the changes on a draft
+        if item and item.is_draft:
+            self.apply_changes(item)
+
+        # Operate directly on a production item
+        else:
+            with changeset_context(author = user):
+
+                if item is None:
+                    item = self.content_type()
+                    redirect = True
+
+                self.apply_changes(item)
+
+        datastore.commit()
+
+        # A new item or draft was created; redirect the browser to it
+        if redirect:
+            raise cherrypy.HTTPRedirect(
+                context["cms"].uri(
+                    context["request"].document.path,
+                    "content", str(item.id)
+                )
+            )
+
+    def _apply_changes(self, item):
+
+        # Save changed fields
+        self.form_adapter.import_object(
+            self.form_data,
+            item,
+            self.form_schema)
+
+        # Drop deleted translations
+        for language in (set(item.translations) - set(self.translations)):
+            del item.translations[language]
+
+    def end(self):
+        
+        if not self.error:
+            self._save_edit_state()
+
+        if not self.redirecting:
+            self.parent.switch_section()
+
+    def _save_edit_state(self):
+        edit_state = self.edit_state
+        edit_state.form_data = self.form_data
+        edit_state.translations = self.translations
+        edit_state.content_type = self.content_type
+
+        edit_states = cherrypy.session["edit_states"]
+        edit_states[edit_state.id] = edit_state
+        cherrypy.session["edit_states"] = edit_states
+
+
