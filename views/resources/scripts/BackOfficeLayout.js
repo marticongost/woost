87db90b1f58bb2645ef4ccf656446993fc10c964
C??sar Ebrat <cesar.ebrat@whads.com>
2008-11-06 12:03:31 +0100

    added control to switch client params

--- views/resources/scripts/BackOfficeLayout.js
+++ views/resources/scripts/BackOfficeLayout.js
@@ -6,35 +6,39 @@ jQuery(document).ready( function () {
              var shortcut = this.shortcut;
              var jqselector = jQuery(this).is("button") ? jQuery(this).attr('id') : jQuery(this).attr('id') + " .label"; 
              var txt = jQuery("#" + jqselector).contents("[nodeType=3]");          
+             
+             if(shortcut){
                           
-             txt.each( function () {
-                if(this.nodeValue.match(new RegExp(shortcut.toUpperCase() + "|" +  shortcut.toLowerCase()))){                     
-                     var texttoshow = document.createElement('span');
-                     var span = document.createElement('span');
-                     var pos = this.nodeValue.indexOf(shortcut.toLowerCase())>0 ? this.nodeValue.indexOf(shortcut.toLowerCase()) : this.nodeValue.indexOf(shortcut.toUpperCase());
-                     span.setAttribute('style','text-decoration: underline;');
-                     span.appendChild(document.createTextNode(this.nodeValue.substring(pos,pos+1)));                     
-                     texttoshow.appendChild(document.createTextNode(this.nodeValue.substring(0,pos)));
-                     texttoshow.appendChild(span);
-                     texttoshow.appendChild(document.createTextNode(this.nodeValue.substring(pos+1)));
-                     jQuery(this).replaceWith(texttoshow);                               
-                }
-               
-             });                                             
-             
-             jQuery("#" + jqselector).attr('title','Alt+Shift+' + shortcut.toUpperCase())
-             
-             jQuery(document).bind(
-                'keydown',
-                {
-                    combi:'Alt+Shift+' + shortcut.toLowerCase(),
-                    disableInInput: false,
-                    extra: jqselector
-                },
-                function (evt){                
-                    jQuery("#" + evt.data.extra).click();                    
-                    return false; 
-             });
+                 txt.each( function () {
+                    if(this.nodeValue.match(new RegExp(shortcut.toUpperCase() + "|" +  shortcut.toLowerCase()))){                     
+                         var texttoshow = document.createElement('span');
+                         var span = document.createElement('span');
+                         var pos = this.nodeValue.indexOf(shortcut.toLowerCase())>0 ? this.nodeValue.indexOf(shortcut.toLowerCase()) : this.nodeValue.indexOf(shortcut.toUpperCase());
+                         span.setAttribute('style','text-decoration: underline;');
+                         span.appendChild(document.createTextNode(this.nodeValue.substring(pos,pos+1)));                     
+                         texttoshow.appendChild(document.createTextNode(this.nodeValue.substring(0,pos)));
+                         texttoshow.appendChild(span);
+                         texttoshow.appendChild(document.createTextNode(this.nodeValue.substring(pos+1)));
+                         jQuery(this).replaceWith(texttoshow);                               
+                    }
+                   
+                 });                                             
+                 
+                 jQuery("#" + jqselector).attr('title','Alt+Shift+' + shortcut.toUpperCase())
+                 
+                 jQuery(document).bind(
+                    'keydown',
+                    {
+                        combi:'Alt+Shift+' + shortcut.toLowerCase(),
+                        disableInInput: false,
+                        extra: jqselector
+                    },
+                    function (evt){                
+                        jQuery("#" + evt.data.extra).click();                    
+                        return false; 
+                 });
+              
+            }
              
         });                   
     
