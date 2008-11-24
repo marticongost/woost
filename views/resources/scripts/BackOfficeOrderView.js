668224774982ab7df30d07b93befebe77457e74f
César Ebrat <cebrat@gmail.com>
2008-11-24 14:05:24 +0100

    sorting elements in the OrderContentView with drag and drop javascript

---
+++ views/resources/scripts/BackOfficeOrderView.js
@@ -0,0 +1,71 @@
+jQuery(document).ready( function () {
+        
+      jQuery(".OrderContentView .Table tbody tr").hover(function() {
+            jQuery(this.cells[0]).addClass('showDragHandle');
+      }, function() {
+            jQuery(this.cells[0]).removeClass('showDragHandle');
+      });
+  
+        
+      jQuery(".OrderContentView .Table tr").each( function (i) {            
+            var td = document.createElement('td');
+            if(i>0) td.className = 'dragHandle';
+            jQuery(this).prepend(td);
+            
+            jQuery(this).attr('id', jQuery(this).find(":checkbox").val());
+                   
+      });  
+    
+      function renderEvenOdd() {
+          jQuery(".OrderContentView .Table tbody tr").each( function (i) {
+                jQuery(this).removeClass();
+                if(i%2) {
+                    jQuery(this).addClass("odd");
+                }else{
+                    jQuery(this).addClass("even");
+                }
+          });
+      }
+      
+      var edit_stack, member, position      
+      
+      // http://marti.whads.com:8080/en/cms/order?member=access_rules_by_priority&item=6&edit_stack=0-1&selection=40
+      
+      jQuery("*", document.body).each( function() {               
+         if (this.edit_stack && typeof(this.edit_stack) == "string") edit_stack = this.edit_stack;                         
+         if (this.member) member = this.member;
+      });  
+    
+      jQuery(".OrderContentView .Table").tableDnD({
+            onDrop: function(table, row) {
+                renderEvenOdd();                
+                
+                jQuery(".OrderContentView .Table tbody tr").each( function (i) {
+                    if(jQuery(row).attr('id') == jQuery(this).attr('id')) position = i;                                          
+                });
+                
+                var url = '/' + cocktail.getLanguage() + '/cms/order?';                           
+                url += "selection=" + jQuery(row).attr('id') + "&";
+                url += "member=" + member + "&";
+                url += "edit_stack=" + edit_stack + "&";
+                url += "action=order&";
+                url += "format=json&";
+                url += "position=" + position;          
+                                                            
+                jQuery.ajax({
+        			url: url,
+        			type: "POST",
+        			data: {},
+        			complete: function(str){
+                        //alert(str.ResponseText);
+        			},
+        			error: function(XMLHttpRequest, textStatus, errorThrown){
+        			
+        			}
+        		});
+                                
+            },
+            dragHandle: "dragHandle",
+            onDragClass: "mydragClass"
+       });
+});
