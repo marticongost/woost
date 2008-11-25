b7d1e89908e85603cb8a114c0901a00e69654114
Mart? Congost <marti.congost@whads.com>
2008-11-25 09:21:11 +0100

    Added missing files from a previous commit

---
+++ views/documenttreecontentview.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			November 2008
+"""
+from cocktail.html import templates
+from sitebasis.models import Site
+
+TreeContentView = templates.get_class("sitebasis.views.TreeContentView")
+
+class DocumentTreeContentView(TreeContentView):
+ 
+    children_collection = "children"
+
+    def __init__(self, *args, **kwargs):
+        TreeContentView.__init__(self, *args, **kwargs)
+        self.root = Site.main.home
+
