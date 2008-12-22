4a2ea86d6cb30e121f00eec5927a33645bbcb6ea
Mart? Congost <marti.congost@whads.com>
2008-12-22 09:43:55 +0100

    Added a package for site specific translations to the project skeleton.

---
+++ sitebasis/scripts/project_skeleton/translations/strings.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			December 2008
+"""
+from cocktail.translations import translations, translate
+
