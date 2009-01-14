c5d45a4d52440f2114180f1bb48b339aaf4782a6
Mart? Congost <marti.congost@whads.com>
2009-01-14 16:56:01 +0100

    Limit filtering of already related items to Collection type members (it's not needed on References)

--- sitebasis/controllers/backoffice/contentcontroller.py
+++ sitebasis/controllers/backoffice/contentcontroller.py
@@ -9,7 +9,7 @@
 from __future__ import with_statement
 import cherrypy
 from cocktail.modeling import getter, cached_getter
-from cocktail.schema import Member, Adapter, Reference, String
+from cocktail.schema import Member, Adapter, Reference, String, Collection
 from cocktail.schema.expressions import CustomExpression
 from cocktail.persistence import datastore
 from cocktail.html.datadisplay import SINGLE_SELECTION, MULTIPLE_SELECTION
@@ -251,7 +251,8 @@ class ContentController(BaseBackOfficeController):
         
         # Exclude items that are already contained on an edited collection
         node = self.stack_node
-        if node and isinstance(node, RelationNode):
+        if node and isinstance(node, RelationNode) \
+        and isinstance(node.member, Collection):
             related_items = self.edit_stack[-2].get_collection(node.member)
             user_collection.add_base_filter(CustomExpression(
                 lambda item: item not in related_items
