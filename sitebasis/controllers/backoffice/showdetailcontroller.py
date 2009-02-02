96d84a42ebc654e380333da09b4664adb9e2a1d2
Mart? Congost <marti.congost@whads.com>
2009-02-02 17:20:30 +0100

    Improvements to PropertyTable

--- sitebasis/controllers/backoffice/showdetailcontroller.py
+++ sitebasis/controllers/backoffice/showdetailcontroller.py
@@ -6,10 +6,19 @@
 @organization:	Whads/Accent SL
 @since:			January 2009
 """
+from cocktail.modeling import cached_getter
+from sitebasis.models import Language
 from sitebasis.controllers.backoffice.editcontroller import EditController
 
 
 class ShowDetailController(EditController):
 
     view_class = "sitebasis.views.BackOfficeShowDetailView"
+
+    @cached_getter
+    def output(self):
+        output = EditController.output(self)
+        # TODO: Add a translation selector
+        output["translations"] = Language.codes
+        return output
 
