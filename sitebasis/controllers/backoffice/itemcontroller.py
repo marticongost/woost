607847169e5d09c9902a319ea8e7eeda47a20c54
Mart? Congost <marti.congost@whads.com>
2009-02-03 18:02:24 +0100

    Edici? aniuada d'elements relacionats ja existents

--- sitebasis/controllers/backoffice/itemcontroller.py
+++ sitebasis/controllers/backoffice/itemcontroller.py
@@ -106,7 +106,9 @@ class ItemController(BaseBackOfficeController):
             redirect = True
 
         # Make sure the top node of the stack is an edit node
-        if not edit_stack or not isinstance(edit_stack[-1], EditNode):
+        if not edit_stack \
+        or not isinstance(edit_stack[-1], EditNode) \
+        or edit_stack[-1].item is not self.edited_item:
             edit_state = EditNode()
             edit_state.item = self.edited_item
             edit_state.content_type = (
