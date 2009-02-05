9ddccf4f5f9fd6fd6b909a383a6cff5156749bf6
Mart? Congost <marti.congost@whads.com>
2009-02-05 17:25:23 +0100

    Import site specific views when starting the application

--- sitebasis/scripts/project_skeleton/scripts/run.py
+++ sitebasis/scripts/project_skeleton/scripts/run.py
@@ -3,6 +3,7 @@
 Launches the site's application server.
 """
 import _PROJECT_MODULE_.models
+import _PROJECT_MODULE_.views
 from _PROJECT_MODULE_.controllers import _PROJECT_NAME_CMS
 
 def main():
