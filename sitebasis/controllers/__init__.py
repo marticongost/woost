89b0122bae469ce02ba439dfe2d29787e1fbcbb3
Anton <Anton@localhost>
2009-02-05 15:16:48 +0100

    Added a controllers.is_accessible() function

--- sitebasis/controllers/__init__.py
+++ sitebasis/controllers/__init__.py
@@ -7,8 +7,13 @@ Controllers for the CMS backend and frontend applications.
 @organization:	Whads/Accent SL
 @since:			July 2008
 """
+from cocktail.controllers import context
 from sitebasis.controllers.application import CMS
 from sitebasis.controllers.module import Module
 from sitebasis.controllers.basecmscontroller import BaseCMSController
 from sitebasis.controllers.defaulthandler import DefaultHandler
+
+def is_accessible(document):
+    return document.is_published() \
+        and context["cms"].allows(action = "read", target_instance = document)
 
