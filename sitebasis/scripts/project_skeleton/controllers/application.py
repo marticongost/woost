2b08c8c1b549e5b319866f646e0328ea5a314613
Mart? Congost <marti.congost@whads.com>
2009-02-05 17:26:14 +0100

    Publish the views/resources/ folder of sites by default

--- sitebasis/scripts/project_skeleton/controllers/application.py
+++ sitebasis/scripts/project_skeleton/controllers/application.py
@@ -2,8 +2,16 @@
 """
 Provides the CMS subclass used to customize the behavior of the site.
 """
+import cherrypy
+from pkg_resources import resource_filename
 from sitebasis.controllers.application import CMS
 
+
 class _PROJECT_NAME_CMS(CMS):
-    pass    
+    
+    class ApplicationContainer(CMS.ApplicationContainer):        
+        _PROJECT_MODULE__resources = cherrypy.tools.staticdir.handler(
+            section = "_PROJECT_MODULE_",
+            dir = resource_filename("_PROJECT_MODULE_.views", "resources")
+        )
 
