5d8701036039ab8c05f9655187e269183e24c87a
Mart? Congost <marti.congost@whads.com>
2009-02-10 17:30:40 +0100

    Added some unit tests for access authorization

---
+++ sitebasis/tests/runner.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			February 2009
+"""
+if __name__ == "__main__":
+    from cocktail.tests.runner import TestSuiteRunner
+    TestSuiteRunner("sitebasis.tests").main()
+
