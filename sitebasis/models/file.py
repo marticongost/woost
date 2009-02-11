244ef63a4160264a42c0410e3bad1dcfab08855b
Mart? Congost <marti.congost@whads.com>
2009-02-11 18:41:45 +0100

    Changed the base class of the File model from Document to Item; adapted the CMS controller accordingly

--- sitebasis/models/file.py
+++ sitebasis/models/file.py
@@ -6,28 +6,68 @@
 @organization:	Whads/Accent SL
 @since:			July 2008
 """
+from datetime import datetime
 from cocktail import schema
-from sitebasis.models.document import Document
+from sitebasis.models.document import Item
 
-class File(Document):
+class File(Item):
  
-    default_handler = "sitebasis.controllers.filehandler.FileHandler"
+    #edit_form = "sitebasis.views.FileForm"
 
-    members_order = "file_path", "file_language", "mime_type", "documents"
-
-    file_path = schema.String(required = True)
-
-    file_language = schema.Reference(
-        type = "sitebasis.models.Language"
-    )
+    members_order = [
+        "title",
+        "description",
+        "enabled",
+        "start_date",
+        "end_date",
+        "external",
+        "location",
+        "mime_type",
+        "documents"
+    ]
     
+    title = schema.String(
+        required = True,
+        translated = True
+    )
+
+    description = schema.String(
+        translated = True,
+        edit_control = "cocktail.html.TextArea"
+    )
+
+    enabled = schema.Boolean(
+        required = True,
+        translated = True,
+        default = True
+    )
+
+    start_date = schema.DateTime(
+        indexed = True
+    )
+
+    end_date = schema.DateTime(
+        indexed = True,
+        min = start_date
+    )
+
+    external = schema.Boolean(required = True)
+    
+    location = schema.String(required = True)
+
     mime_type = schema.String(
-        required = True
+        required = external.not_()
     )
 
     documents = schema.Collection(
         items = "sitebasis.models.Document",
         bidirectional = True
     )
-  
+
+    def is_published(self, language = None):
+        return (
+            self.get("enabled", language)
+            and (self.start_date is None or self.start_date <= datetime.now())
+            and (self.end_date is None or self.end_date > datetime.now())
+        )
 
