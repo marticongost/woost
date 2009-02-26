65936a4ce129e1c06de599e41309f491bfc3653e
Mart? Congost <marti.congost@whads.com>
2009-02-26 11:09:58 +0100

    Set the application_path attribute of generated CMS subclasses

--- sitebasis/scripts/project_skeleton/controllers/application.py
+++ sitebasis/scripts/project_skeleton/controllers/application.py
@@ -9,8 +9,8 @@ from sitebasis.controllers.application import CMS
 
 class _PROJECT_NAME_CMS(CMS):
 
-    upload_path = resource_filename("_PROJECT_MODULE_", "upload")
-    
+    application_path = resource_filename("_PROJECT_MODULE_", None)
+        
     class ApplicationContainer(CMS.ApplicationContainer):        
         _PROJECT_MODULE__resources = cherrypy.tools.staticdir.handler(
             section = "_PROJECT_MODULE_",
