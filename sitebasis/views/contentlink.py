e108fd9462e63f21229bddc7af519fe907d963de
Mart? Congost <marti.congost@whads.com>
2009-03-02 09:20:59 +0100

    Make BackOfficeShowDetailView, ContentDisplayMixin, ContentList and ContentLink propagate the edit stack

--- sitebasis/views/contentlink.py
+++ sitebasis/views/contentlink.py
@@ -14,6 +14,7 @@ class ContentLink(Element):
 
     item = None
     base_url = None
+    edit_stack = None
 
     def _ready(self):
 
@@ -21,7 +22,10 @@ class ContentLink(Element):
 
         if self.item and self.base_url:
             self.tag = "a"
-            self["href"] = self.base_url + "/content/%s" % self.item.id
+            self["href"] = \
+                self.base_url + "/content/%s/show_detail" % self.item.id
+            if self.edit_stack:
+                self["href"] += "?edit_stack=" + self.edit_stack.to_param()
             self.append(translate(self.item))
         else:
             self.append(u"-")
