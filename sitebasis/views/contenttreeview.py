603d7348470180c4eff51e1e2d9c7770da3734dc
Mart? Congost <marti.congost@whads.com>
2009-03-02 17:20:25 +0100

    Add a ContentTreeView class

---
+++ sitebasis/views/contenttreeview.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			March 2009
+"""
+from cocktail.html import templates
+
+TreeView = templates.get_class("cocktail.html.TreeView")
+
+
+class ContentTreeView(TreeView):
+
+    authorization_check = None
+    base_url = None
+    edit_stack = None
+
+    def filter(self, item):
+        if self.authorization_check:
+            return self.authorization_check(target_instance = item)
+
+    def create_label(self, item):
+        label = templates.new("sitebasis.views.ContentLink")
+        label.base_url = self.base_url
+        label.item = item
+        label.edit_stack = self.edit_stack
+        return label
+
