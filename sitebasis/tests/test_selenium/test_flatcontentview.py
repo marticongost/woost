4db27a927f65edc5febe40d10978a31ba4acd600
Mart? Congost <marti.congost@whads.com>
2009-05-25 15:51:44 +0200

    Added a selenium test for element sorting

--- sitebasis/tests/test_selenium/test_flatcontentview.py
+++ sitebasis/tests/test_selenium/test_flatcontentview.py
@@ -21,6 +21,49 @@ class FlatContentViewTestCase(object):
         assert browser.jquery_count(".collection_display tbody tr") == all
 
     @selenium_test
+    def test_sort_by_id(self):
+        
+        browser.open(
+            "/en/cms/content/?content_view=flat"
+            "&page_size=%d"
+            "&members=element&members=id"   
+            % len(Item.select())
+        )
+        admin_login()
+
+        def get_browser_ids():
+            return browser.get_eval(
+                "var ids = [];"
+                "window.jQuery('.collection_display tbody td.id_column')"
+                ".each(function () {"
+                "   ids.push(window.jQuery(this).text());"
+                "});"
+                "ids.join(' ');"
+            )
+
+        # Show the column dropdown panel and click the ascending order button
+        browser.click("css=.collection_display th.id_column .selector a.label")
+        browser.click(
+            "css=.collection_display th.id_column .sorting_options "
+            "a.ascending"
+        )
+        browser.wait_for_page_to_load(10000)
+        sorted_ids = " ".join(str(x.id) for x in Item.select(order = "id"))
+        print sorted_ids
+        print get_browser_ids()
+        assert get_browser_ids() == sorted_ids
+
+        # Show the column dropdown panel and click the descending order button
+        browser.click("css=.collection_display th.id_column .selector a.label")
+        browser.click(
+            "css=.collection_display th.id_column .sorting_options "
+            "a.descending"
+        )
+        browser.wait_for_page_to_load(10000)
+        sorted_ids = " ".join(str(x.id) for x in Item.select(order = "-id"))
+        assert get_browser_ids() == sorted_ids
+
+    @selenium_test
     def test_selection_tools(self):
 
         browser.open("/en/cms/content/?content_view")
