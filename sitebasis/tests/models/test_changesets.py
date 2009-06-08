9a02c849b1ec1c4cdaa9cbe91350f88ffad6395a
Jordi Fern?ndez <jordi.fernandez@whads.com>
2009-06-08 13:01:33 +0200

    Created a base class for the tests and modified all the existing tests to inherit from it.

--- sitebasis/tests/models/test_changesets.py
+++ sitebasis/tests/models/test_changesets.py
@@ -7,24 +7,18 @@ u"""
 @since:			February 2009
 """
 from __future__ import with_statement
-from unittest import TestCase
-from cocktail.tests.persistence.tempstoragemixin import TempStorageMixin
+from sitebasis.tests.models.basetestcase import BaseTestCase
 
 
-class ChangeSetTests(TempStorageMixin, TestCase):
+class ChangeSetTests(BaseTestCase):
 
     def test_insert(self):
 
         from datetime import datetime
         from sitebasis.models import (
-            Site, Document, User, Action, ChangeSet, changeset_context
+            Document, User, ChangeSet, changeset_context
         )
 
-        Site(qname = "sitebasis.main_site").insert()
-
-        create = Action(identifier = "create")
-        create.insert()
-        
         author = User()
         author.insert()
 
@@ -33,57 +27,45 @@ class ChangeSetTests(TempStorageMixin, TestCase):
             document.set("title", u"Test document title", "en")
             document.set("inner_title", u"Test document inner title", "en")
             document.hidden = True
-            self.assertFalse(changeset.changes)
+            assert not changeset.changes
             document.insert()
         
-        self.assertEqual(list(ChangeSet.select()), [changeset])
-        self.assertTrue(changeset.author is author)
-        self.assertTrue(isinstance(changeset.date, datetime))
+        assert list(ChangeSet.select()) == [changeset]
+        assert changeset.author is author
+        assert isinstance(changeset.date, datetime)
         
-        self.assertEqual(changeset.changes.keys(), [document.id])
+        assert changeset.changes.keys() == [document.id]
         change = changeset.changes[document.id]
-        self.assertTrue(change.target is document)
-        self.assertTrue(change.action is create)
-        self.assertTrue(change.changeset is changeset)
-        self.assertEqual(document.changes, [change])
+        assert change.target is document
+        assert change.action is self.create_action
+        assert change.changeset is changeset
+        assert document.changes == [change]
 
         for key in "id", "changes", "creation_time", "last_update_time":
-            self.assertFalse(key in change.item_state)
+            assert not key in change.item_state
 
-        self.assertEqual(
-            change.item_state["title"],
-            {"en": u"Test document title"}
-        )
+        assert change.item_state["title"] == {"en": u"Test document title"}
 
-        self.assertEqual(
-            change.item_state["inner_title"],
+        assert change.item_state["inner_title"] == \
             {"en": u"Test document inner title"}
-        )
 
-        self.assertTrue(change.item_state["hidden"])
+        assert change.item_state["hidden"]
 
-        self.assertEqual(
-            change.item_state["enabled"],
+        assert change.item_state["enabled"] == \
             {"en": document.get("enabled", "en")}
-        )
 
-        self.assertTrue(document.author is author)
-        self.assertTrue(document.owner is author)
-        self.assertTrue(document.creation_time)
-        self.assertTrue(document.last_update_time)
-        self.assertEqual(document.creation_time, document.last_update_time)
+        assert document.author is author
+        assert document.owner is author
+        assert document.creation_time
+        assert document.last_update_time
+        assert document.creation_time == document.last_update_time
 
     def test_delete(self):
 
         from datetime import datetime
         from sitebasis.models import (
-            Site, Item, User, Action, ChangeSet, changeset_context
+            Item, User, ChangeSet, changeset_context
         )
-
-        Site(qname = "sitebasis.main_site").insert()
-
-        delete = Action(identifier = "delete")
-        delete.insert()
 
         author = User()
         author.insert()
@@ -95,35 +77,27 @@ class ChangeSetTests(TempStorageMixin, TestCase):
             item.owner = None
             item.delete()
         
-        self.assertEqual(list(ChangeSet.select()), [changeset])
-        self.assertTrue(changeset.author is author)
-        self.assertTrue(isinstance(changeset.date, datetime))
+        assert list(ChangeSet.select()) == [changeset]
+        assert changeset.author is author
+        assert isinstance(changeset.date, datetime)
         
-        self.assertEqual(changeset.changes.keys(), [item.id])
+        assert changeset.changes.keys() == [item.id]
         change = changeset.changes[item.id]
-        self.assertTrue(change.target is item)
-        self.assertTrue(change.action is delete)
-        self.assertTrue(change.changeset is changeset)
-        self.assertEqual(item.changes, [change])
+        assert change.target is item
+        assert change.action is self.delete_action
+        assert change.changeset is changeset
+        assert item.changes == [change]
 
-        self.assertFalse(item.id in Item.index)
+        assert not item.id in Item.index
 
     def test_modify(self):
 
         from time import sleep
         from datetime import datetime
         from sitebasis.models import (
-            Site, Document, User, Action, ChangeSet, changeset_context
+            Document, User, ChangeSet, changeset_context
         )
 
-        Site(qname = "sitebasis.main_site").insert()
-
-        create = Action(identifier = "create")
-        create.insert()
-        
-        modify = Action(identifier = "modify")
-        modify.insert()
-        
         author = User()
         author.insert()
 
@@ -142,44 +116,34 @@ class ChangeSetTests(TempStorageMixin, TestCase):
             document.set("inner_title", u"Test document new inner title", "en")
             document.hidden = True
 
-        self.assertEqual(list(ChangeSet.select()), [creation, modification])
-        self.assertTrue(modification.author is author)
-        self.assertTrue(isinstance(modification.date, datetime))
+        assert list(ChangeSet.select()) == [creation, modification]
+        assert modification.author is author
+        assert isinstance(modification.date, datetime)
         
-        self.assertEqual(modification.changes.keys(), [document.id])
+        assert modification.changes.keys() == [document.id]
         change = modification.changes[document.id]
-        self.assertTrue(change.target is document)
-        self.assertTrue(change.action is modify)
-        self.assertTrue(change.changeset is modification)
-        self.assertEqual(change.changed_members, set(["title", "inner_title"]))
-        self.assertEqual(
-            document.changes,
-            [creation.changes[document.id], change]
-        )
+        assert change.target is document
+        assert change.action is self.modify_action
+        assert change.changeset is modification
+        assert change.changed_members == set(["title", "inner_title"])
+        assert document.changes == [creation.changes[document.id], change]
 
         for key in "id", "changes", "creation_time", "last_update_time":
-            self.assertFalse(key in change.item_state)
+            assert not key in change.item_state
 
-        self.assertEqual(
-            change.item_state["title"],
-            {"en": u"Test document new title"}
-        )
+        assert change.item_state["title"] == {"en": u"Test document new title"}
 
-        self.assertEqual(
-            change.item_state["inner_title"],
+        assert change.item_state["inner_title"] == \
             {"en": u"Test document new inner title"}
-        )
 
-        self.assertTrue(change.item_state["hidden"])
+        assert change.item_state["hidden"]
 
-        self.assertEqual(
-            change.item_state["enabled"],
+        assert change.item_state["enabled"] == \
             {"en": document.get("enabled", "en")}
-        )
 
-        self.assertTrue(document.author is author)
-        self.assertTrue(document.owner is author)
-        self.assertTrue(document.creation_time)
-        self.assertTrue(document.last_update_time)
-        self.assertNotEqual(document.creation_time, document.last_update_time)
+        assert document.author is author
+        assert document.owner is author
+        assert document.creation_time
+        assert document.last_update_time
+        assert not document.creation_time == document.last_update_time
 
