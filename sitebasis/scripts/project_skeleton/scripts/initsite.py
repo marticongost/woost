67166f79dff493a2520898c2ac734068ef894bd0
Mart? Congost <marti.congost@whads.com>
2009-06-29 17:55:56 +0200

    Add shebang lines to site scripts

--- sitebasis/scripts/project_skeleton/scripts/initsite.py
+++ sitebasis/scripts/project_skeleton/scripts/initsite.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 u"""
 Initializes the site's database.
--- sitebasis/scripts/project_skeleton/scripts/shell.py
+++ sitebasis/scripts/project_skeleton/scripts/shell.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 u"""A convenience script that can be launched through ipython or vanilla python
 with the -i flag, to have quick access to the project's data.
