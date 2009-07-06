debec00f2b028b06818925f857216ff0a1aaa9fc
Mart? Congost <marti.congost@whads.com>
2009-07-06 15:27:46 +0200

    Use a dropdown selector to edit TriggerResponse.target_state

--- sitebasis/extensions/workflow/triggerresponse.py
+++ sitebasis/extensions/workflow/triggerresponse.py
@@ -18,10 +18,10 @@ class SetStateTriggerResponse(TriggerResponse):
     target_state = schema.Reference(
         type = State,
 	    required = True,
-	    related_end = schema.Collection(cascade_delete = True)
+	    related_end = schema.Collection(cascade_delete = True),
+        edit_control = "cocktail.html.DropdownSelector"
     )
 
     def execute(self, item, action, agent, batch = False, **context):
-
         item.state = self.target_state
 
