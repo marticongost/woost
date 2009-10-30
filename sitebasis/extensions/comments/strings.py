2efe8f12843eb4e4683c2f8a76840ab9f5af2705
Jordi Fern?ndez <jordi.fernandez@whads.com>
2009-10-30 14:05:18 +0100

    Created an extension that adds support for comments.

---
+++ sitebasis/extensions/comments/strings.py
@@ -0,0 +1,62 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Jordi Fernández
+@contact:		jordi.fernandez@whads.com
+@organization:	Whads/Accent SL
+@since:			October 2009
+"""
+from cocktail.translations import translations
+
+# Comment
+#------------------------------------------------------------------------------
+translations.define("Comment",
+    ca = u"Comentari",
+    es = u"Comentario",
+    en = u"Comment"
+)
+
+translations.define("Comment-plural",
+    ca = u"Comentaris",
+    es = u"Comentarios",
+    en = u"Comments"
+)
+
+translations.define("Comment.document",
+    ca = u"Document",
+    es = u"Documento",
+    en = u"Document"
+)
+
+translations.define("Comment.user_name",
+    ca = u"Nom",
+    es = u"Nombre",
+    en = u"Name"
+)
+
+translations.define("Comment.user_email",
+    ca = u"Correu electrònic",                                                  
+    es = u"Correo electrónico",
+    en = u"E-mail address"
+)
+
+translations.define("Comment.content",
+    ca = u"Comentari",                                                  
+    es = u"Comentario",
+    en = u"Comment"
+)
+
+# Document
+#------------------------------------------------------------------------------
+translations.define("Document.allow_comments",
+    ca = u"Accepta comentaris",
+    es = u"Acepta comentarios",
+    en = u"Comments allowed"
+)
+
+translations.define("Document.comments",
+    ca = u"Comentaris",
+    es = u"Comentarios",
+    en = u"Comments"
+)
+
