5aa5619c512c260f4b84c0a43dcc91b5f62f3fa5
Jordi Fern?ndez <jordi.fernandez@whads.com>
2009-12-15 16:01:13 +0100

    Implemented ResourceIsPublishedExpression and ResourceIsAccessibleExpression expressions and some useful methods like is_current, is_accessible and select_accessible.

--- sitebasis/models/resource.py
+++ sitebasis/models/resource.py
@@ -11,6 +11,9 @@ from cocktail.modeling import getter, abstractmethod
 from cocktail.translations import translations
 from cocktail import schema
 from sitebasis.models.item import Item
+from sitebasis.models.usersession import get_current_user
+from sitebasis.models.permission import ReadPermission
+from sitebasis.models.expressions import IsPublishedExpression, IsAccessibleExpression
 
 class Resource(Item):
 
@@ -80,10 +83,38 @@ class Resource(Item):
         return (self.draft_source is None and self.get("title", language)) \
             or Item.__translate__(self, language, **kwargs)
 
+    def is_current(self):
+        now = datetime.now()
+        return (self.start_date is None or self.start_date <= now) \
+            and (self.end_date is None or self.end_date > now)
+
     def is_published(self, language = None):
         return (
             self.get("enabled", language)
             and (self.start_date is None or self.start_date <= datetime.now())
             and (self.end_date is None or self.end_date > datetime.now())
         )
+
+    def is_accessible(self, user = None, language = None):
+        return self.is_published(language) \
+            and (user or get_current_user()).has_permission(
+                ReadPermission,
+                target = self
+            )
+
+    @classmethod
+    def select_accessible(cls, *args, **kwargs):
+        return cls.select(filters = [
+            ResourceIsAccessibleExpression(get_current_user())
+        ]).select(*args, **kwargs)
+
+
+class ResourceIsPublishedExpression(IsPublishedExpression):
+    """An expression that tests if resources are published."""
+    content_type = Resource
+
+
+class ResourceIsAccessibleExpression(IsAccessibleExpression):
+    """An expression that tests that resources can be accessed by a user."""
+    content_type = Resource
 
