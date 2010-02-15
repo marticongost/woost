f85d5a15ac5985895b4d56c05b7ef0f12974e46d
Jordi Fern?ndez <jordi.fernandez@whads.com>
2010-02-15 13:27:28 +0100

    Changed the way to check if a form is submitted. Submit button is not named 'submit' since revision 904 of cocktail.

--- woost/extensions/workflow/transitioncontroller.py
+++ woost/extensions/workflow/transitioncontroller.py
@@ -79,8 +79,7 @@ class TransitionController(FormControllerMixin, BaseBackOfficeController):
     @cached_getter
     def form_errors(self):
 
-        if "cancel" in cherrypy.request.params \
-        and "submit" not in cherrypy.request.params:
+        if "cancel" in cherrypy.request.params:
            return schema.ErrorList([])
         else:
             return FormControllerMixin.form_errors(self)
@@ -99,7 +98,8 @@ class TransitionController(FormControllerMixin, BaseBackOfficeController):
         item = self.item
         draft_source = item.draft_source
 
-        if "submit" in cherrypy.request.params:
+        if cherrypy.request.method == "POST" \
+        and not "cancel" in cherrypy.request.params:
 
             # Apply form data
             FormControllerMixin.submit(self)
