1e6f1a9c76fef8c093a037ed4ba1b9eb4abd236d
cesar <cesar@wa-cesar>
2010-03-05 14:43:00 +0100

    add new renderpreviewcontroller and remove the exposed method who makes the preview render

--- woost/controllers/backoffice/backofficecontroller.py
+++ woost/controllers/backoffice/backofficecontroller.py
@@ -33,6 +33,8 @@ from woost.controllers.backoffice.contentcontroller \
 from woost.controllers.backoffice.deletecontroller import DeleteController
 from woost.controllers.backoffice.ordercontroller import OrderController
 from woost.controllers.backoffice.movecontroller import MoveController
+from woost.controllers.backoffice.renderpreviewcontroller \
+        import RenderPreviewController
 from woost.controllers.backoffice.changelogcontroller \
     import ChangeLogController
 
@@ -52,6 +54,7 @@ class BackOfficeController(BaseBackOfficeController):
     order = OrderController
     move = MoveController
     changelog = ChangeLogController
+    render_preview = RenderPreviewController
     
     def submit(self):
         raise cherrypy.HTTPRedirect(
@@ -81,55 +84,6 @@ class BackOfficeController(BaseBackOfficeController):
             
             if edit_stack is not None:
                 edit_stacks_manager.preserve_edit_stack(edit_stack)
-
-    @cherrypy.expose
-    def render_preview(self, **kwargs):
-        
-        node = self.stack_node
-        
-        get_current_user().require_permission(
-            ReadPermission,
-            target = node.item
-        )
-        
-        node.import_form_data(
-            node.form_data,
-            node.item
-        )
-        
-        errors = list(node.item.__class__.get_errors(node.item))
-
-        if errors:
-            message = Element("div",
-                class_name = "preview-error-box",
-                children = [
-                    translations(
-                        "woost.backoffice invalid item preview", 
-                        get_content_language()
-                    ),
-                    Element("ul", children = [
-                        Element("li", children = [translations(error)])
-                        for error in errors
-                    ])
-                ]
-            )
-            message.add_resource("/resources/styles/backoffice.css")           
-            return message.render_page()        
-        else:
-            self.context.update(
-                original_publishable = self.context["publishable"],
-                publishable = node.item
-            )
-            
-            controller = node.item.resolve_controller()
-
-            if controller is None:
-                raise cherrypy.NotFound()
-
-            if isinstance(controller, type):
-                controller = controller()
-
-            return controller()
 
     @cherrypy.expose
     def editor_attachments(self, **kwargs):
