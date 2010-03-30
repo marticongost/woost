abb5bb0cb3516c69544237fb42f8def7e8501b38
Jordi Fern?ndez <jordi.fernandez@whads.com>
2010-03-30 11:51:37 +0200

    Reimplementation of staticsite extension.

--- woost/extensions/staticsite/__init__.py
+++ woost/extensions/staticsite/__init__.py
@@ -14,7 +14,6 @@ from cocktail.persistence import datastore
 from cocktail.controllers import context
 from woost.models import (
     Publishable,
-    PublicationScheme,
     Extension,
     Document,
     File
@@ -30,18 +29,6 @@ translations.define("StaticSiteExtension-plural",
     ca = u"Lloc web estàtic",
     es = u"Sitio web estático",
     en = u"Static website"
-)
-
-translations.define("StaticSiteExtension.encoding",
-    ca = u"Codificació dels fitxers",
-    es = u"Codificación de los ficheros",
-    en = u"File encoding"
-)
-
-translations.define("StaticSiteExtension.index_file_name",
-    ca = u"Nom dels fitxers d'índex",
-    es = u"Nombre de los ficheros de índice",
-    en = u"Folder index file name"
 )
 
 translations.define("StaticSiteExtension.file_extension",
@@ -69,29 +56,6 @@ class StaticSiteExtension(Extension):
             "en"
         )
 
-    members_order = [
-        "encoding",
-        "index_file_name"
-    ]
-
-    encoding = schema.String(
-        required = True,
-        default = "utf-8"
-    )
-
-    index_file_name = schema.String(
-        required = True,
-        default = "index"
-    )
-    
-    root_redirection_template = """
-<html>
-    <head>
-        <meta http-equiv="refresh" content="0;url=%s">
-    </head>
-    <body></body>
-</html>
-"""
 
     @event_handler
     def handle_loading(cls, event):
@@ -104,32 +68,22 @@ class StaticSiteExtension(Extension):
         from woost.extensions.staticsite import (
             useraction,
             strings,
-            exportstaticsitecontroller,
-            staticpublicationscheme
+            staticsitesnapshooter,
+            exportationpermission,
+            exportstaticsitecontroller 
         )
 
-        from woost.extensions.staticsite.staticsiteexporter \
-            import StaticSiteExporter
-    
+        from woost.extensions.staticsite.staticsitedestination \
+            import StaticSiteDestination
+
         BackOfficeController.export_static = \
             exportstaticsitecontroller.ExportStaticSiteController
-
-        StaticSiteExtension.add_member(
-            schema.Reference(
-                "publication_scheme",
-                type = PublicationScheme,
-                bidirectional = True,
-                integral = True,
-                required = True,
-                related_end = schema.Reference()
-            )
-        )
-
+    
         StaticSiteExtension.add_member(
             schema.Collection(
-                "exporters",
+                "destinations",
                 items = schema.Reference(
-                    type = StaticSiteExporter,                    
+                    type = StaticSiteDestination,                    
                 ),
                 bidirectional = True,
                 integral = True,
@@ -137,67 +91,6 @@ class StaticSiteExtension(Extension):
                 min = 1
             )
         )
-
-        Publishable.add_member(
-            schema.Boolean("exportable_as_static_content",
-                default = False,
-                required = True,
-                listed_by_default = False,
-                member_group = "publication"
-            ),
-            after = "translation_enabled"
-        )
-
-        Document.default_exportable_as_static_content = True
-        File.default_exportable_as_static_content = True
-
-        for qname in (
-            "woost.login_page",
-            "woost.backoffice",
-            "woost.webservices"
-        ):
-            item = Publishable.get_instance(qname = qname)
-            item.exportable_as_static_content = False
-
-        if extension.publication_scheme is None:
-            pubscheme = staticpublicationscheme.StaticPublicationScheme()
-            extension.publication_scheme = pubscheme
-            pubscheme.insert()
-
-        datastore.commit()
-
-        # Override the publication scheme during the export process
-        from woost.models import Site, PathResolution
-
-        def resolve_path(self, path):
-            if context.get("exporting_static_site", False):
-                if not path:
-                    # TODO: index.html?
-                    return PathResolution(None, self.home)
-                else:
-                    resolution = extension.publication_scheme.resolve_path(path)
-                    if resolution is not None:
-                        return resolution
-            else:
-                return base_resolve_path(self, path)
-
-        base_resolve_path = Site.resolve_path
-        Site.resolve_path = resolve_path
-
-        def get_path(self, publishable, language = None):
-            if context.get("exporting_static_site", False):
-                if publishable is self.home:
-                    # TODO: index.html?
-                    return ""
-                
-                path = extension.publication_scheme.get_path(publishable, language)
-                if path is not None:
-                    return path
-            else:
-                return base_get_path(self, publishable, language)
-
-        base_get_path = Site.get_path
-        Site.get_path = get_path
 
         # Disable interactive features from rendered pages when rendering
         # static content
