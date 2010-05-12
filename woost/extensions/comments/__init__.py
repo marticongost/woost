6436ddb980b2376fff9c73f0e6fb7a29223e34dc
César Ebrat <cebrat@gmail.com>
2010-05-12 17:15:18 +0200

    remake some lost changes in local merge to comments extension

--- woost/extensions/comments/__init__.py
+++ woost/extensions/comments/__init__.py
@@ -57,12 +57,12 @@ class CommentsExtension(Extension):
         )
 
     def _after_process_comments(self):
-        raise cherrypy.HTTPRedirect(unicode(Location.get_current()))
+        raise cherrypy.HTTPRedirect(unicode(Location.get_current()).encode('utf-8'))
 
     @event_handler
     def handle_loading(cls, event):
         
-        # Import the extension's models
+        # Import the extension's <models></models>
         from woost.extensions.comments import strings
         from woost.extensions.comments.comment import Comment
 
@@ -162,7 +162,7 @@ class CommentsExtension(Extension):
                     or not member.editable
                     or not issubclass(member.schema, comment_model)
                 )
-                adapter.exclude("publishable")
+                adapter.exclude(["publishable","captcha"])
 
                 comments_schema = schema.Schema(comment_model.name + "Form")
                 adapter.export_schema(comment_model, comments_schema)
@@ -192,15 +192,14 @@ class CommentsExtension(Extension):
 
                         if not comment_errors:
                             comment = comment_model()
-                            comment_id = comment.id
 
                             adapter.import_object(                                                                                                                                                                       
                                 comment_data,
                                 comment,
-                                source_schema = comment_model
+                                source_schema = comments_schema,
+                                target_schema = comment_model
                             )
 
-                            comment.id = comment_id
                             comment.publishable = publishable
                             comment.insert()
                             datastore.commit()
