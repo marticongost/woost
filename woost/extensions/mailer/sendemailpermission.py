e8dd70660659c602951a5395fb877b862ca5de82
Jordi Fern?ndez <jordi.fernandez@whads.com>
2010-10-06 09:50:54 +0200

    Added a collection with the authorized roles to the SendEmailPermission.

--- woost/extensions/mailer/sendemailpermission.py
+++ woost/extensions/mailer/sendemailpermission.py
@@ -6,11 +6,28 @@
 @organization:	Whads/Accent SL
 @since:			September 2010
 """
+from cocktail import schema
 from woost.models.permission import Permission
+from woost.models.messagestyles import permission_doesnt_match_style
 
 
 class SendEmailPermission(Permission):
     """Permission to send an email."""
 
     instantiable = True
+
+    roles = schema.Collection(
+        items = schema.Reference(type = "woost.models.Role"),
+        related_key = schema.Collection()
+    )
+
+    def match(self, role = None, verbose = False):
+
+        if role not in self.roles:
+            print permission_doesnt_match_style("Role doesn't match")
+            return False
+
+        return Permission.match(self, verbose)
+
+    
 
