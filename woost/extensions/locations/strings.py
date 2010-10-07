e7f268451e233aa2bc22cb973201401f96151796
Mart? Congost <marti.congost@whads.com>
2010-10-07 17:13:06 +0200

    Added the 'locations' extension

---
+++ woost/extensions/locations/strings.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from cocktail.translations import translations
+
+# Location
+#------------------------------------------------------------------------------
+translations.define("Location",
+    ca = u"Localitat",
+    es = u"Localidad",
+    en = u"Location"
+)
+
+translations.define("Location-plural",
+    ca = u"Localitats",
+    es = u"Localidades",
+    en = u"Locations"
+)
+
+translations.define("Location.location_name",
+    ca = u"Nom",
+    es = u"Nombre",
+    en = u"Name"
+)
+
+translations.define("Location.code",
+    ca = u"Codi",
+    es = u"Código",
+    en = u"Code"
+)
+
+translations.define("Location.parent",
+    ca = u"Localitat pare",
+    es = u"Localidad padre",
+    en = u"Parent location"
+)
+
+translations.define("Location.locations",
+    ca = u"Localitats filles",
+    es = u"Localidades hijas",
+    en = u"Child locations"
+)
+
