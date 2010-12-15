24dfc56e8480779d0315fd42664a1c4e59b17dde
Mart? Congost <marti.congost@whads.com>
2010-12-15 18:20:28 +0100

    Adapt the webconsole integration code to API changes in the upstream project

--- woost/extensions/webconsole/cmswebconsole.py
+++ woost/extensions/webconsole/cmswebconsole.py
@@ -4,31 +4,33 @@ u"""
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
 import cherrypy
-from webconsole import WebConsole
+from webconsole import WebConsoleController
 from cocktail.events import event_handler
 from cocktail.iteration import first, last
 from cocktail.translations import translations, get_language
 from cocktail.persistence import PersistentObject
+from cocktail.controllers import request_property
 from woost.controllers.documentcontroller import DocumentController
 from woost.models import Site, get_current_user
 from woost.extensions.webconsole.webconsolepermission \
     import WebConsolePermission
 
 
-class CMSWebConsole(DocumentController, WebConsole):
+class CMSWebConsole(DocumentController, WebConsoleController):
 
     @event_handler
     def handle_traversed(self, event):
         get_current_user().require_permission(WebConsolePermission)
 
-    submit = WebConsole.submit
-    submitted = WebConsole.submitted
+    submit = WebConsoleController.submit
+    submitted = WebConsoleController.submitted
 
-    def _update_eval_context(self, context):
+    @request_property
+    def session(self):
 
-        WebConsole._update_eval_context(self, context)
+        session = WebConsoleController.session(self)
 
-        context.update(
+        session.context.update(
             site = Site.main,
             user = get_current_user(),
             language = get_language(),
@@ -38,5 +40,7 @@ class CMSWebConsole(DocumentController, WebConsole):
         )
 
         for model in PersistentObject.schema_tree():
-            context[model.name] = model
+            session.context[model.name] = model
+
+        return session
 
