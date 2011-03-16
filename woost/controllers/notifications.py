76a0e7ab2eaa688280a6539a8e8a5f9bd19c50ef
Mart? Congost <marti.congost@whads.com>
2011-03-16 17:53:13 +0100

    Added a 'filter' parameter to the pop_user_notifications() and the NotificationBox class

--- woost/controllers/notifications.py
+++ woost/controllers/notifications.py
@@ -33,12 +33,25 @@ def notify_user(message, category = None, transient = True):
 
     notifications.append((message, category, transient))
 
-def pop_user_notifications():
+def pop_user_notifications(filter = None):
     """Retrieves pending notification messages that were stored through the
     L{notify_user} method.
 
     Retrieved messages are considered to be consumed, and therefore they
     are removed from the list of pending notifications.
+
+    @param filter: If given, only those notifications matching the specified
+        criteria will be retrieved. The criteria matches as follows:
+
+            * If set to a string, it designates a single notification category
+              to retrieve.
+            * If set to a sequence (list, tuple, set), it designates a set of
+              categories; notifications belonging to any of the specified
+              categories will be retrieved.
+            * If set to a callable, it will be used as filter function, taking
+              a notification tuple as its single parameter, and returning True
+              if the notification is to be retrieved, or False if it should be
+              ignored.
 
     @return: The sequence of pending notification messages. Each message
         consists of a tuple with the message text, its category and wether
@@ -46,6 +59,33 @@ def pop_user_notifications():
     @rtype: sequence of (tuple of (unicode, unicode or None, bool))
     """
     notifications = session.get("notifications")
-    session["notifications"] = []
-    return notifications or []
+
+    if notifications is None:
+        return []
+    
+    remaining = []
+
+    if filter:
+        matching = []
+
+        if isinstance(filter, basestring):
+            match = lambda notification: notification[1] == filter
+        elif callable(filter):
+            match = filter
+        elif hasattr("__contains__", filter):
+            match = lambda notification: notification[1] in filter
+        else:
+            raise TypeError(
+                "The 'filter' parameter for the pop_user_notifications() "
+                "function should be a string, a sequence or callable, got "
+                "%s instead" % type(filter)
+            )
+
+        for notification in notifications:
+            (matching if match(notification) else remaining).append(notification)
+
+        notifications = matching    
+
+    session["notifications"] = remaining
+    return notifications
 
