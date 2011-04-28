a098b828989697b4cd9d83b976db51b9e334c581
Mart? Congost <marti.congost@whads.com>
2011-04-28 12:24:53 +0200

    Improvements to the user interface for the Facebook publication form

---
+++ woost/views/resources/scripts/facebookpublication-FacebookPublicationView.js
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------------
+
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			April 2011
+-----------------------------------------------------------------------------*/
+
+cocktail.bind(".FacebookPublicationView", function ($view) {
+
+    var $resultsTable = $view.find(".results_table");
+
+    if ($resultsTable.length) {
+        var $form = $view.find(".publication_form")
+
+        var $submitButton = $form.find(".buttons button:last");
+        $submitButton.hide();
+        
+        var $fields = $view.find(".publication_form .fields");
+        $fields.hide();
+
+        var $publishAgainLink = jQuery(cocktail.instantiate(
+            "woost.extensions.facebookpublication.FacebookPublicationView.publish_again_link"
+        ));
+        $publishAgainLink.click(function () {
+            $publishAgainLink.remove();
+            $submitButton.show();
+            $fields.show();
+            return false;
+        });
+        $submitButton.before($publishAgainLink);
+    }
+
+    $view.find(".publication_form").submit(function () {
+        
+        var $form = jQuery(this);
+
+        var $sign = jQuery(cocktail.instantiate(
+            "woost.extensions.facebookpublication.FacebookPublicationView.loading_sign"
+        ));
+        $form.append(jQuery("<div class='disabling_layer'>"))
+        $form.before($sign);
+
+        $resultsTable.fadeOut();
+        return true;
+    });
+});
+
