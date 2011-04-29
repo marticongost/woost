7d207f0e4c119316c52e09ae45210743756cd2ca
Jordi Fern?ndez <jordi.fernandez@whads.com>
2011-04-29 11:47:11 +0200

    Added an event fired when an order is completed and a new trigger type raised with this event to send an email with the order summary.

--- woost/extensions/ecommerce/summarycontroller.py
+++ woost/extensions/ecommerce/summarycontroller.py
@@ -10,13 +10,8 @@ from cocktail.controllers import (
     request_property,
     FormProcessor
 )
-from woost.models import (
-    Publishable,
-    get_current_user,
-    ModifyMemberPermission
-)
+from woost.models import Publishable
 from woost.controllers.documentcontroller import DocumentController
-from woost.extensions.ecommerce.ecommerceorder import ECommerceOrder
 from woost.extensions.ecommerce.basket import Basket
 from woost.extensions.ecommerce.orderstepcontroller import (
     ProceedForm,
@@ -29,31 +24,8 @@ class SummaryController(FormProcessor, DocumentController):
 
     @request_property
     def checkout_schema(self):
-        checkout_schema = schema.Schema("OrderCheckoutSummary")
-        self.checkout_adapter.export_schema(
-            Basket.get().__class__,
-            checkout_schema
-        )
-        return checkout_schema
+        return Basket.get().get_summary_schema()
         
-    @request_property
-    def checkout_adapter(self):
-        user = get_current_user()            
-        adapter = schema.Adapter()
-        adapter.exclude(["customer", "status", "purchases"])
-        adapter.exclude([
-            member.name
-            for member in Basket.get().__class__.members().itervalues()
-            if not member.visible
-            or not member.editable
-            or not issubclass(member.schema, ECommerceOrder)
-            or not user.has_permission(
-                ModifyMemberPermission,
-                member = member
-            )
-        ])
-        return adapter
-
     @request_property
     def output(self):
         output = DocumentController.output(self)
@@ -74,7 +46,9 @@ class SummaryController(FormProcessor, DocumentController):
             payments = PaymentsExtension.instance
             
             # Redirect the user to the payment gateway
-            if payments.enabled and payments.payment_gateway:
+            if payments.enabled \
+            and payments.payment_gateway \
+            and order.payment_type == "credit_card":
                 payments.payment_request(order.id)
             
             # No payment gateway available, redirect the user to the success
