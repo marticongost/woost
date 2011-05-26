c6b9815b8787d581a6fa21d3aa2a00ab55f1af2f
Mart? Congost <marti.congost@whads.com>
2011-05-26 10:06:24 +0200

    Hide EcommerceBillingConcept in the root menu

--- woost/extensions/ecommerce/ecommercebillingconcept.py
+++ woost/extensions/ecommerce/ecommercebillingconcept.py
@@ -38,6 +38,8 @@ class ECommerceBillingConcept(Item):
         "implementation"
     ]
     
+    visible_from_root = False
+
     title = schema.String(
         translated = True,
         descriptive = True,
