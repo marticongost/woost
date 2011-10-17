e869550071a21428e65724e343f51d79d3ba858a
Mart? Congost <marti.congost@whads.com>
2011-10-17 10:33:18 +0200

    When requesting image URLs using Publishable.get_image_uri(), include file extensions by default (added an 'include_extension' parameter to disable this behavior)

--- woost/models/publishable.py
+++ woost/models/publishable.py
@@ -362,18 +362,27 @@ class Publishable(Item):
         return uri
 
     def get_image_uri(self,
-        effects = None,
+        image_factory = None,
         parameters = None,
         host = None,
-        encode = True):
+        encode = True,
+        include_extension = True):
                 
         uri = make_uri("/images", self.id)
 
-        if effects:
-            if isinstance(effects, basestring):
-                uri = make_uri(uri, effects)
-            else:
-                uri = make_uri(uri, *effects)
+        if image_factory:
+            uri = make_uri(uri, image_factory)
+
+        if include_extension:
+            from woost.models.rendering.formats import (
+                formats_by_extension,
+                extensions_by_format,
+                default_format
+            )
+            ext = getattr(self, "file_extension", None)
+            if ext is None or ext not in formats_by_extension:
+                ext = extensions_by_format[default_format]
+            uri += "." + ext
 
         if parameters:
             uri = make_uri(uri, **parameters)
