900c94be760c6fbaa2677729e98806ea639dc224
Mart? Congost <marti.congost@whads.com>
2011-11-30 12:19:56 +0100

    Moved several features of woost.views.Menu to cocktail.html.TreeView

--- woost/views/menu.py
+++ woost/views/menu.py
@@ -16,14 +16,6 @@ TreeView = templates.get_class("cocktail.html.TreeView")
 class Menu(TreeView):
     """A visual component used to render navigation menus and site maps.
     
-    @var root_visible: If set to True, the produced tree has a single root.
-        Otherwise, the root element is hidden and the tree appears to have
-        multiple roots instead.
-    @type root_visible: bool
-
-    @var selection: Indicates the selected item.
-    @type selection: L{Publishable<woost.models.publishable.Publishable>}
-
     @var emphasized_selection: When set to True, adds a <strong> tag to
         highlight the selected item.
     @type emphasized_selection: bool
@@ -35,22 +27,12 @@ class Menu(TreeView):
     @var linked_containers: Indicates if entries that contain other entries
         should behave as links.
     @type linked_containers: bool
-
-    @var max_depth: The maximum depth for the menu.
-    @type max_depth: int
-
-    @var expanded: When set to True, the menu will be completely expanded. When
-        False, entries will only be expanded if they contain the selected
-        entry.
-    @type expanded: bool
     """
-    root_visible = False
-    selection = None
+    root_visibility = TreeView.HIDDEN_ROOT
+    expanded = False
     emphasized_selection = True
     linked_selection = True
     linked_containers = True
-    max_depth = None
-    expanded = False
 
     def _ready(self):
 
@@ -59,18 +41,7 @@ class Menu(TreeView):
 
         if self.selection is None:
             self.selection = context["publishable"]
-        
-        # Find the selected path
-        self._expanded = set()
-        item = self.selection
-
-        while item is not None:
-            self._expanded.add(item)
-            item = self.get_parent_item(item)
-
-        # Limited depth
-        self._depth = 2 if self.root_visible else 1
-
+       
         TreeView._ready(self)
     
     def filter_item(self, item):
@@ -78,14 +49,6 @@ class Menu(TreeView):
 
     def get_item_uri(self, item):
         return item.get_uri(host = ".")
-
-    def create_entry(self, item):
-        entry = TreeView.create_entry(self, item)
-
-        if item in self._expanded:
-            entry.add_class("selected")
-
-        return entry
 
     def create_label(self, item):
         
@@ -108,17 +71,4 @@ class Menu(TreeView):
     def should_link(self, item):
         return (self.linked_selection or item is not self.selection) \
             and (self.linked_containers or not item.children)
-
-    def get_child_items(self, parent):
-
-        if (self.max_depth is not None and self._depth > self.max_depth) \
-        or (not self.expanded and parent not in self._expanded):
-            return []
-        else:
-            return getattr(parent, "children", [])
-
-    def _fill_children_container(self, container, item, children):
-        self._depth += 1
-        TreeView._fill_children_container(self, container, item, children)
-        self._depth -= 1        
 
