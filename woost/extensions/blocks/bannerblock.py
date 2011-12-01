f73a106cbdcadc28873b4096370de0df7d7d4f14
Mart? Congost <marti.congost@whads.com>
2011-12-01 10:35:13 +0100

    Blocks extension: added an option to have image-only banners

--- woost/extensions/blocks/bannerblock.py
+++ woost/extensions/blocks/bannerblock.py
@@ -14,7 +14,12 @@ class BannerBlock(Block):
     tag = "a"
     view_class = "woost.extensions.blocks.Banner"
 
-    members_order = "target", "image", "text"
+    members_order = [
+        "target",
+        "image",
+        "text",
+        "label_displayed"
+    ]
 
     image = schema.Reference(
         type = File,
@@ -32,7 +37,12 @@ class BannerBlock(Block):
 
     text = schema.String(
         translated = True,
-        edit_control = "woost.views.RichTextEditor",
+        member_group = "content"
+    )
+
+    label_displayed = schema.Boolean(
+        default = True,
+        required = True,
         member_group = "content"
     )
 
@@ -41,4 +51,7 @@ class BannerBlock(Block):
         view.image = self.image
         view.target = self.target
         view.text = self.text
-        
+        view.label_displayed = self.label_displayed
+        from cocktail.styled import styled
+        print styled(view, "green"), view.label_displayed
+
