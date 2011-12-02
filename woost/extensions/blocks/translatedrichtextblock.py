8ad6f4b2e0e39f0587d247d52daf042c4f0f158d
Mart? Congost <marti.congost@whads.com>
2011-12-02 13:05:52 +0100

    Blocks extension: added members to TranslatedRichTextBlock to make it easier to define image+text blocks

--- woost/extensions/blocks/translatedrichtextblock.py
+++ woost/extensions/blocks/translatedrichtextblock.py
@@ -4,7 +4,9 @@ u"""
 .. moduleauthor:: Jordi Fernández <jordi.fernandez@whads.com>
 """
 from cocktail import schema
-from woost.models import Publishable
+from cocktail.html import Element, templates
+from woost.models import Publishable, File
+from woost.models.rendering import ImageFactoryMember
 from woost.extensions.blocks.block import Block
 
 
@@ -13,10 +15,23 @@ class TranslatedRichTextBlock(Block):
     instantiable = True
     view_class = "cocktail.html.Element"
 
+    members_order = ["text", "image", "image_factory", "attachments"]
+
     text = schema.String(
         required = True,
         edit_control = "woost.views.RichTextEditor",
         translated = True,
+        member_group = "content"
+    )
+
+    image = schema.Reference(
+        type = File,
+        related_end = schema.Collection(),
+        relation_constraints = [File.resource_type.equal("image")],
+        member_group = "content"
+    )
+
+    image_factory = ImageFactoryMember(
         member_group = "content"
     )
 
@@ -27,6 +42,17 @@ class TranslatedRichTextBlock(Block):
     )
 
     def init_view(self, view):
-        Block.init_view(self, view)        
-        view.append(self.text)
+        Block.init_view(self, view)
+
+        if self.image is None:
+            view.append(self.text)
+        else:
+            image = templates.new("woost.views.Image")
+            image.image = self.image
+            image.image_factory = self.image_factory
+            view.append(image)
+
+            text_container = Element()
+            text_container.append(self.text)
+            view.append(text_container)
 
