3863b6f5a973d8043afed65b9bc21a249af2f21e
Mart? Congost <marti.congost@whads.com>
2011-12-12 12:41:38 +0100

    Don't raise SelectionError as an unhandled exception; instead, notify the user and go back to the previous backoffice location. Also, provide a mechanism to do the same for other user action exceptions.

--- woost/controllers/backoffice/basebackofficecontroller.py
+++ woost/controllers/backoffice/basebackofficecontroller.py
@@ -19,7 +19,10 @@ from cocktail.controllers import get_parameter, CookieParameterSource
 from woost.models import Item
 from woost.controllers import BaseCMSController
 from woost.controllers.notifications import notify_user
-from woost.controllers.backoffice.useractions import get_user_action
+from woost.controllers.backoffice.useractions import (
+    get_user_action,
+    SelectionError
+)
 from woost.controllers.backoffice.editstack import (
     EditNode,
     SelectionNode,
@@ -177,9 +180,18 @@ class BaseBackOfficeController(BaseCMSController):
 
     def _invoke_user_action(self, action, selection):
         for error in action.get_errors(self, selection):
-            raise error
+            self._handle_user_action_error(action, selection, error)
 
         action.invoke(self, selection)
+
+    _graceful_user_action_errors = set([SelectionError])
+
+    def _handle_user_action_error(self, action, selection, error):
+        if isinstance(error, tuple(self._graceful_user_action_errors)):
+            notify_user(translations(error), "error")
+            self.go_back()
+        else:
+            raise error
 
     def _get_user_action(self, param_key = "action"):
         action = None
