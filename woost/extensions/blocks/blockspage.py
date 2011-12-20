701df81d51bf3dfb52f1c13bdd07e4fa8439ea7a
Mart? Congost <marti.congost@whads.com>
2011-12-20 09:27:19 +0100

    Backed out changeset: ef5b77a808d7 (it could conflict with nested edits and custom types derived from StandardPage or BlocksPage)

--- woost/extensions/blocks/blockspage.py
+++ woost/extensions/blocks/blockspage.py
@@ -6,14 +6,9 @@ u"""Defines the `BlocksPage` model.
 from cocktail import schema
 from woost.models import Document, Template
 from woost.extensions.blocks.block import Block
-from woost.models.site import Site
 
 
 class BlocksPage(Document):
-
-    default_parent = schema.DynamicDefault(
-        lambda: Site.main.home
-    )
 
     blocks = schema.Collection(
         items = schema.Reference(type = Block),        
