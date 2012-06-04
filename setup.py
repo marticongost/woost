f7fd1a5fe239eff06a33ac206ce48680854c5b01
Mart? Congost <marti.congost@whads.com>
2012-06-04 09:29:38 +0200

    Bumped the version to 0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os.path import join, isdir
 
 setup(
     name = "woost",
-    version = "0.8",
+    version = "0.9",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     maintainer = "Marti Congost",
