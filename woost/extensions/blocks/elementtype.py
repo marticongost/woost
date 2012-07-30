b540916d49a7aada83dbdb99f3d02392cea2ba3a
Mart? Congost <marti.congost@whads.com>
2012-07-30 09:43:22 +0200

    blocks extension: added the ElementType member type, use it in TextBlock and ContainerBlock

---
+++ woost/extensions/blocks/elementtype.py
@@ -0,0 +1,44 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from cocktail.translations import translations
+from cocktail import schema
+
+
+class ElementType(schema.String):
+
+    required = True
+    default = "section"
+    enumeration = [
+        "div",
+        "section",
+        "article",
+        "details",
+        "aside",
+        "header",
+        "footer",
+        "nav",
+        "dd"
+    ]
+
+    def translate_value(self, value, language = None, **kwargs):
+
+        translation = schema.String.translate_value(
+            self, 
+            value,
+            language,
+            **kwargs
+        )
+
+        if (
+            not translation 
+            and value 
+            and self.translatable_enumeration 
+            and self.enumeration
+        ):
+            translation = translations("woost.extensions.blocks.ElementType=" + value)
+
+        return translation
+
