81325dca3d507cf8f3e9b144de823d463381cb59
Mart? Congost <marti.congost@whads.com>
2012-08-06 17:25:35 +0200

    blocks extension: force shallow copying for nested common blocks when deep copying a block structure.

--- woost/extensions/blocks/block.py
+++ woost/extensions/blocks/block.py
@@ -182,7 +182,7 @@ class Block(Item):
             and member.is_persistent_relation
             and issubclass(member.related_type, Block)
         ):
-            mode = schema.DEEP_COPY
+            mode = lambda block, member, value: not block.is_common_block
 
         return mode
 
