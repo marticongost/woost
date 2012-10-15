4c69968c4bc37563a244f60fd004e344d457b570
Mart? Congost <marti.congost@whads.com>
2012-10-15 12:39:02 +0200

    shorturls extension: added a reference to an URL shortener to Site, to define a site-wide URL shortening service

---
+++ woost/extensions/shorturls/site.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from cocktail import schema
+from woost.models import Site
+from woost.extensions.shorturls.urlshortener import URLShortener
+
+
+Site.add_member(
+    schema.Reference("url_shortener",
+        type = URLShortener,
+        related_end = schema.Reference(),
+        member_group = "behavior.publication",
+        after_member = "caching_policies"
+    )
+)
+
