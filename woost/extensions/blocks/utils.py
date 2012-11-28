cc420c1f58aa22843fdc1ac4e8d032beb33d4c8e
Mart? Congost <marti.congost@whads.com>
2012-11-28 09:32:27 +0100

    blocks extension: Discarded Block.descend_blocks(), replaced with the more general find_blocks() function in the utils module

--- woost/extensions/blocks/utils.py
+++ woost/extensions/blocks/utils.py
@@ -3,6 +3,8 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+from woost.extensions.blocks.block import Block
+from woost.extensions.blocks.slot import Slot
 
 def create_block_views(blocks):
     for block in blocks:
@@ -10,4 +12,21 @@ def create_block_views(blocks):
             view = block.create_view()
             if view is not None:
                 yield view
+
+def find_blocks(obj, slots = None):
+
+    if isinstance(obj, Block):
+        yield obj
+
+    if slots is None:
+        slots = (member
+                for member in obj.__class__.members().itervalues()
+                if isinstance(member, Slot))
+
+    for slot in slots:
+        blocks = obj.get(slot)
+        if blocks is not None:
+            for block in blocks:
+                for descendant in find_blocks(block):
+                    yield descendant
 
