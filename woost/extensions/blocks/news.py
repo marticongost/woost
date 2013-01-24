1060e942e137266e07fec8ee183a6fbd95c99c4b
Mart? Congost <marti.congost@whads.com>
2013-01-24 13:31:52 +0100

    blocks extension: removed unnecessary import statements

--- woost/extensions/blocks/news.py
+++ woost/extensions/blocks/news.py
@@ -3,9 +3,7 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from cocktail import schema
 from woost.models import News
-from woost.extensions.blocks.block import Block
 from woost.extensions.blocks.slot import Slot
 
 # Replace the News.body text member with a list of blocks
