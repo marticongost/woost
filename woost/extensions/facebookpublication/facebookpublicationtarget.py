e89ec37079f93214b226492bd095341ef5ddf432
Mart? Congost <marti.congost@whads.com>
2013-01-28 10:03:50 +0100

    facebookpublication extension: adapted to changes in the Facebook graph API

--- woost/extensions/facebookpublication/facebookpublicationtarget.py
+++ woost/extensions/facebookpublication/facebookpublicationtarget.py
@@ -19,56 +19,57 @@ from woost.models import Item, Language
 from woost.extensions.opengraph import OpenGraphExtension
 
 facebook_locales = {
-    "Catalan": "1",
-    "Czech": "2",
-    "Welsh": "3",
-    "Danish": "4",
-    "German": "5",
-    "English (Upside Down)": "51",
-    "English (US)": "6",
-    "Spanish": "23",
-    "Spanish (Spain)": "7",
-    "Finnish": "8",
-    "French (France)": "9",
-    "Hungarian": "30",
-    "Italian": "10",
-    "Japanese": "11",
-    "Korean": "12",
-    "Norwegian (bokmal)": "13",
-    "Dutch": "14",
-    "Polish": "15",
-    "Portuguese (Brazil)": "16",
-    "Portuguese (Portugal)": "31",
-    "Romanian": "32",
-    "Russian": "17",
-    "Slovak": "33",
-    "Slovenian": "34",
-    "Swedish": "18",
-    "Thai": "35",
-    "Turkish": "19",
-    "Simplified Chinese (China)": "20",
-    "Traditional Chinese (Hong Kong)": "21",
-    "Traditional Chinese (Taiwan)": "22",
-    "Afrikaans": "36",
-    "Bengali": "45",
-    "Bulgarian": "37",
-    "Croatian": "38",
-    "English (UK)": "24",
-    "French (Canada)": "44",
-    "Greek": "39",
-    "Hindi": "46",
-    "Indonesian": "25",
-    "Lithuanian": "40",
-    "Malay": "41",
-    "Punjabi": "47",
-    "Serbian": "42",
-    "Filipino": "26",
-    "Tamil": "48",
-    "Telugu": "49",
-    "Malayalam": "50",
-    "Vietnamese": "27",
-    "Arabic": "28",
-    "Hebrew": "29"    
+    "Catalan": 1,
+    "Czech": 2,
+    "Welsh": 3,
+    "Danish": 4,
+    "German": 5,
+    "English (Upside Down)": 51,
+    "English (US)": 6,
+    "Spanish": 23,
+    "Spanish (Spain)": 7,
+    "Spanish (All)": 1002,
+    "Finnish": 8,
+    "French (France)": 9,
+    "Hungarian": 30,
+    "Italian": 10,
+    "Japanese": 11,
+    "Korean": 12,
+    "Norwegian (bokmal)": 13,
+    "Dutch": 14,
+    "Polish": 15,
+    "Portuguese (Brazil)": 16,
+    "Portuguese (Portugal)": 31,
+    "Romanian": 32,
+    "Russian": 17,
+    "Slovak": 33,
+    "Slovenian": 34,
+    "Swedish": 18,
+    "Thai": 35,
+    "Turkish": 19,
+    "Simplified Chinese (China)": 20,
+    "Traditional Chinese (Hong Kong)": 21,
+    "Traditional Chinese (Taiwan)": 22,
+    "Afrikaans": 36,
+    "Bengali": 45,
+    "Bulgarian": 37,
+    "Croatian": 38,
+    "English (UK)": 24,
+    "French (Canada)": 44,
+    "Greek": 39,
+    "Hindi": 46,
+    "Indonesian": 25,
+    "Lithuanian": 40,
+    "Malay": 41,
+    "Punjabi": 47,
+    "Serbian": 42,
+    "Filipino": 26,
+    "Tamil": 48,
+    "Telugu": 49,
+    "Malayalam": 50,
+    "Vietnamese": 27,
+    "Arabic": 28,
+    "Hebrew": 29    
 }
 
 
@@ -181,23 +182,21 @@ class FacebookPublicationTarget(Item):
                 "publishable": publishable,
                 "locales": facebook_locales,
                 "include_locales": (
-                    lambda included: ",".join(
-                        facebook_locales[loc_name] for loc_name in included
-                    )
+                    lambda included:
+                        [facebook_locales[loc_name] for loc_name in included]
                 ),
                 "exclude_locales": (
-                    lambda excluded: ",".join(
-                        loc_id 
+                    lambda excluded:
+                        [loc_id 
                         for loc_name, loc_id in facebook_locales.iteritems()
-                        if loc_name not in excluded
-                    )
+                        if loc_name not in excluded]
                 )
             }
             targeting_code = self.targeting.replace("\r\n", "\n")
             exec targeting_code in context
             targeting = context.get("targeting")
             if targeting:
-                post_data["targeting"] = dumps(targeting)
+                post_data["feed_targeting"] = dumps(targeting)
 
         return post_data
 
