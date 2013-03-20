bf4ac84bdcc8d1f097ab0a2be3ae3460680b8728
Jordi Fern?ndez <jordi.fernandez@whads.com>
2013-03-20 12:51:06 +0100

    Discard the host in Item._fix_uri when the uri part has a scheme

--- woost/models/uri.py
+++ woost/models/uri.py
@@ -64,9 +64,6 @@ class URI(Publishable):
             if parameters:
                 uri = make_uri(uri, **parameters)
 
-            if host == "?" or host == "!":
-                host = None
-
             uri = self._fix_uri(uri, host, encode)
 
         return uri
