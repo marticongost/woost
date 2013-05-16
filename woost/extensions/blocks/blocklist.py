89e36d6f9255c03a7bd2adab4e8c1b0219cd008e
Mart? Congost <marti.congost@whads.com>
2013-05-16 16:26:03 +0200

    blocks extension: make it easier to wrap each block in a BlockList with different elements; implemented a default wrapping for table-based block lists

--- woost/extensions/blocks/blocklist.py
+++ woost/extensions/blocks/blocklist.py
@@ -9,6 +9,8 @@ from woost.extensions.blocks.utils import create_block_views
 
 class BlockList(Element):
 
+    __wrap = None
+
     def __init__(self, *args, **kwargs):
         Element.__init__(self, *args, **kwargs)
         first_last_classes(self)
@@ -18,7 +20,12 @@ class BlockList(Element):
 
     def _ready(self):
         Element._ready(self)
-        self.__is_list = self.tag in ("ul", "ol")
+
+        if self.tag in ("ul", "ol"):
+            self.__wrap = self.wrap_with_list_item
+        elif self.tag == "table":
+            self.__wrap = self.wrap_with_table_row
+
         self._fill_blocks()
 
     def _fill_blocks(self):
@@ -27,10 +34,21 @@ class BlockList(Element):
                 self._insert_block_view(block_view)
 
     def _insert_block_view(self, block_view):
-        if self.__is_list:
-            entry = Element("li")
-            entry.append(block_view)
+        if self.__wrap:
+            entry = self.__wrap(block_view)
             self.blocks_container.append(entry)
         else:
             self.blocks_container.append(block_view)
+
+    def wrap_with_list_item(self, block_view):
+        entry = Element("li")
+        entry.append(block_view)
+        return entry
+
+    def wrap_with_table_row(self, block_view):
+        row = Element("tr")
+        row.cell = Element("td")
+        row.cell.append(block_view)
+        row.append(row.cell)
+        return row
 
