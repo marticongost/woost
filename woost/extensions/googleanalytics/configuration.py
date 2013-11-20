52066554461cca88753c69c803e4ede4b3cc634e
Mart? Congost <marti.congost@whads.com>
2013-11-20 12:10:12 +0100

    googleanalytics extension: do not synchronize the Analytics account across site installations

--- woost/extensions/googleanalytics/configuration.py
+++ woost/extensions/googleanalytics/configuration.py
@@ -10,6 +10,7 @@ Configuration.add_member(
     schema.String("google_analytics_account",
         text_search = False,
         member_group = "services.google_analytics",
+        synchronizable = False,
         listed_by_default = False
     )
 )
