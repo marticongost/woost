b4f31184f36276301940d1c5c873b1b8ef2aa53a
Mart? Congost <marti.congost@whads.com>
2014-01-08 09:17:31 +0100

    Added the models.utils.delete_history() function

--- woost/models/utils.py
+++ woost/models/utils.py
@@ -3,8 +3,11 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from cocktail.persistence.utils \
-    import remove_broken_type as cocktail_remove_broken_type
+from cocktail.persistence import datastore
+from cocktail.persistence.utils import (
+    is_broken,
+    remove_broken_type as cocktail_remove_broken_type
+)
 from .item import Item
 from .configuration import Configuration
 from .changesets import Change
@@ -29,4 +32,17 @@ def remove_broken_type(
         relations = relations,
         languages = languages
     )
+
+def delete_history():
+
+    for item in Item.select():
+        if not is_broken(item):            
+            try:
+                del item._changes
+            except AttributeError:
+                pass
+
+    for key in list(datastore.root):
+        if key.startswith("woost.models.changesets."):
+            del datastore.root[key]
 
