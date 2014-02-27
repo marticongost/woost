b56d8d8f627e31cd75521f8cbeab3d111bd0324a
Mart? Congost <marti.congost@whads.com>
2014-02-27 11:05:25 +0100

    Another redesign of the item translations selector.

--- woost/views/resources/scripts/ItemTranslationsSelector.js
+++ woost/views/resources/scripts/ItemTranslationsSelector.js
@@ -8,100 +8,81 @@
 -----------------------------------------------------------------------------*/
 
 cocktail.bind(".ItemTranslationsSelector", function ($selector) {
-
-    var $checks = $selector.find(".entry input[type=checkbox]");
     
-    $checks.each(function () {
-        this.initialCheckState = this.checked;
-    });
+    var $searchControls = jQuery(cocktail.instantiate("woost.views.ItemTranslationsSelector.searchControls"))
+        .prependTo(this);
 
-    this.revertChanges = function () {
-        $checks.each(function () {
-            this.checked = this.initialCheckState;
-        });
-        $selector.find(".revert_button").click();
-    }
-
-    var $addedInput = jQuery("<input type='hidden' name='add_translation'>")
-        .appendTo($selector);
-
-    var $deletedInput = jQuery("<input type='hidden' name='delete_translation'>")
-        .appendTo($selector);
-
-    function getLocales(entryFilter) {    
-        var locales = [];
-        $selector.find(".check_list "  + entryFilter).each(function () {            
-            locales.push(this.getAttribute("data-locale"));
-        });
-        return locales.join(",");
-    }
-    
-    function recordAddedTranslations() {
-        $addedInput.val(getLocales(".added_translation"));
-    }
-
-    function recordDeletedTranslations() {
-        $deletedInput.val(getLocales(".deleted_translation"));
-    }
-
-    $selector.find(".add_translation_button")
-        .click(function () {
-            jQuery(this).closest(".entry").addClass("added_translation");
-            recordAddedTranslations();
-            return false;
-        });
-
-    $selector.find(".delete_translation_button")
-        .click(function () {
-            jQuery(this).closest(".entry").addClass("deleted_translation");
-            recordDeletedTranslations();
-            return false;
-        });
-
-    $selector.find(".check_list .entry")
-        .each(function () {
-            jQuery(cocktail.instantiate("woost.views.ItemTranslationsSelector.revert_button"))
-                .appendTo(this);
-        })
-        .on("click", ".revert_button", function () {
-            var $entry = jQuery(this).closest(".entry");
-            if ($entry.is(".added_translation")) {
-                $entry.removeClass("added_translation");
-                recordAddedTranslations();
-            }
-            else if ($entry.is(".deleted_translation")) {
-                $entry.removeClass("deleted_translation");
-                recordDeletedTranslations();
-            }
-        });
-
-    $selector.find(".defined_translations_filter input[type=checkbox]")
-        .change(function () {
-            if (this.checked) {
-                $selector.addClass("defined_translations_only");
-            }
-            else {
-                $selector.removeClass("defined_translations_only");
-            }
-        })
+    var $searchBox = $searchControls.find(".search_box")
         .keydown(function (e) {
-            // Up key
-            if (e.keyCode == 38) {
-                $selector.find(".search_box").focus();
+            if (e.keyCode == 40) {
+                $selector.find(".defined_languages_section .language_list").get(0).focusContent();
                 return false;
             }
-            // Down key
-            else if (e.keyCode == 40) {
-                $selector.find(".check_list").get(0).focusContent();
+        });
+
+    var $languageLists = $selector.find(".language_list");
+    
+    $languageLists.each(function (index) {
+
+        var checkList = this;
+        var $checkList = jQuery(this);
+        checkList.topControl = $searchBox.get(0);
+
+        var $selectionControls = jQuery(cocktail.instantiate("woost.views.ItemTranslationsSelector.selectionControls"))
+            .insertAfter(this);
+
+        var $selectAllLink = $selectionControls.find(".select_all_link")
+            .click(function () {
+                checkList.selectEntries(":selectable-entry");
+            });
+
+        var $selectNoneLink = $selectionControls.find(".select_none_link")
+            .click(function () {
+                checkList.clearSelection(":selectable-entry");
+            });
+
+        function toggleSelectionLinks() {
+            var $allEntries = checkList.getEntries(":selectable-entry");
+            var $selectedEntries = $allEntries.filter(".selected");
+
+            if ($allEntries.length > $selectedEntries.length) {
+                $selectAllLink.removeAttr("disabled");
+            }
+            else {
+                $selectAllLink.attr("disabled", "disabled");
+            }
+
+            if ($selectedEntries.length) {
+                $selectNoneLink.removeAttr("disabled");
+            }
+            else {
+                $selectNoneLink.attr("disabled", "disabled");
+            }
+        }
+
+        $selector.on("searched", toggleSelectionLinks);
+        $checkList.on("selectionChanged", toggleSelectionLinks);        
+
+        $checkList.on("keydown", ".entry", function (e) {
+            var sibling = null;
+            if (e.keyCode == 37) {
+                if (index > 0) {
+                    sibling = $languageLists.get(index - 1);
+                }
+            }
+            else if (e.keyCode == 39) {
+                if (index + 1 < $languageLists.length) {
+                    sibling = $languageLists.get(index + 1);
+                }
+            }
+            if (sibling) {
+                sibling.focusContent();
+                return false;
             }
         });
-});
-
-cocktail.bind(".DropdownPanel", function ($dropdown) {
-    $dropdown.on("collapsed", function () {
-        $dropdown.find(".ItemTranslationsSelector").each(function () {
-            this.revertChanges();
-        });
     });
+
+    cocktail.searchable(this);
+    this.applySearch($searchControls.find(".search_box").val());
 });
 
