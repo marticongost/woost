0020515553359bec0235e44e28e304eddfc7ce72
Mart? Congost <marti.congost@whads.com>
2014-03-25 09:19:08 +0100

    Upgraded TinyMCE to the 4.x version.
    As part of the upgrade, the TinyMCE distribution now should be placed in the
    resources/scripts folder of the Cocktail package, not in Woost (it was placed
    in Woost for historical reasons, but obviously that made no sense).
    
    Among other things, the upgrade makes it possible to copy HTML between editor
    instances (the code will be sanitized). On the other hand, the ability to
    synchronize the resizing of editors for different translations has been lost
    as a result of the upgrade (the previous implementation was an extremely fragile
    piece of monkey patching abuse), so resizing has been disabled.

--- woost/views/resources/scripts/RichTextEditor.js
+++ woost/views/resources/scripts/RichTextEditor.js
@@ -7,127 +7,39 @@
 @since:			October 2008
 -----------------------------------------------------------------------------*/
 
-cocktail.declare("woost");
+cocktail.declare("woost.richTextEditor");
 
-woost.initRichTextEditor = function (instance) {
+woost.richTextEditor.badTags = /^(acronym|applet|basefont|big|center|dir|font|frame|frameset|isindex|noframes|script|tt)$/i;
+woost.richTextEditor.badAttributes = /^(style|align|valign|link|alink|vlink|text|background|bgcolor|border|cellpadding|cellspacing|marginheight|marginwidth|nowrap|rules|scrolling|type|frameborder|on.+|data-.+)$/i;
 
-    // Paste as plain text by default
-    instance.pasteAsPlainText = true;
+woost.richTextEditor.init = function (editor) {
 
-    var doc = instance.contentDocument;
-
-    var evento = tinymce.dom.Event;
-
-	evento.remove(instance.id + '_resize', 'mousedown');
-
-	evento.add(instance.id + '_resize', 'mousedown', function(e) {
-
-        var ed = instance;
-        var extend = tinymce.extend;
-
-        ed.settings = s = extend({
-            theme_advanced_resizing_use_cookie : 1,
-            theme_advanced_resizing : true
-        }, ed.settings);
-
-        var DOM = tinymce.DOM;
-        var c, p, w, h, n, pa;
-
-        // Measure container
-        c = DOM.get(ed.id + '_tbl');
-        w = c.clientWidth;
-        h = c.clientHeight;
-
-        miw = s.theme_advanced_resizing_min_width || 100;
-        mih = s.theme_advanced_resizing_min_height || 100;
-        maw = s.theme_advanced_resizing_max_width || 0xFFFF;
-        mah = s.theme_advanced_resizing_max_height || 0xFFFF;
-
-        // Setup placeholder
-        p = DOM.add(DOM.get(ed.id + '_parent'), 'div', {'class' : 'mcePlaceHolder'});
-        DOM.setStyles(p, {width : w, height : h});
-
-        // Replace with placeholder
-        DOM.hide(c);
-        DOM.show(p);
-
-        // Create internal resize obj
-        r = {
-            x : e.screenX,
-            y : e.screenY,
-            w : w,
-            h : h,
-            dx : null,
-            dy : null
-        };
-
-        // Start listening
-        mf = evento.add(document, 'mousemove', function(e) {
-            var w, h;
-
-            // Calc delta values
-            r.dx = e.screenX - r.x;
-            r.dy = e.screenY - r.y;
-
-            // Boundery fix box
-            w = Math.max(miw, r.w + r.dx);
-            h = Math.max(mih, r.h + r.dy);
-            w = Math.min(maw, w);
-            h = Math.min(mah, h);
-
-            // Resize placeholder
-            if (s.theme_advanced_resize_horizontal)
-                p.style.width = w + 'px';
-
-            p.style.height = h + 'px';
-
-            return evento.cancel(e);
-        });
-
-        me = evento.add(document, 'mouseup', function(e) {
-
-            // Stop listening
-            evento.remove(document, 'mousemove', mf);
-            evento.remove(document, 'mouseup', me);
-
-            var c = DOM.get(ed.id + '_tbl');
-
-            c.style.display = '';
-            DOM.remove(p);
-
-            if (r.dx === null) {
-                return;
-            }
-            
-            var Cookie = tinymce.util.Cookie;
-
-            jQuery(".field_instance-RichTextEditor:visible textarea").each(function () {
-
-                var areaid = jQuery(this).attr('id');
-
-                var c = DOM.get(areaid + '_tbl');
-
-                var ifr = DOM.get(areaid + '_ifr');
-
-                if (s.theme_advanced_resize_horizontal)
-                    c.style.width = (r.w + r.dx) + 'px';
-
-                c.style.height = (r.h + r.dy) + 'px';
-                var hc = ifr.clientHeight + r.dy
-                ifr.style.height = (ifr.clientHeight + r.dy) + 'px';
-
-                if (s.theme_advanced_resizing_use_cookie) {
-                    Cookie.setHash("TinyMCE_" + areaid + "_size", {
-                        cw : r.w + r.dx,
-                        ch : r.h + r.dy,
-                        ci : hc
-                    });
+    function cleanDOM(node) {
+        if (node.nodeType == 1) {
+            if (woost.richTextEditor.badTags.test(node.tagName)) {
+                if (node.parentNode) {
+                    node.parentNode.removeChild(node);
                 }
-            });
-        });
+                else {
+                    node.tagName = "div";
+                }
+            }
+            else {
+                for (var a = node.attributes.length - 1; a >= 0; a--) {
+                    var name = node.attributes[a].nodeName;
+                    if (woost.richTextEditor.badAttributes.test(name)) {
+                        node.removeAttribute(name);
+                    }
+                }
+                for (var c = node.childNodes.length - 1; c >= 0; c--) {
+                    cleanDOM(node.childNodes[c]);
+                }
+            }
+        }
+    }
 
-        return evento.cancel(e);
-	});
-
+    editor.on("PastePostProcess", function (e) {
+        cleanDOM(e.node);
+    });
 }
 
