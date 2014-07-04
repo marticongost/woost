4a1027d719f740dda8adf28609fce53cd3ee3560
Mart? Congost <marti.congost@whads.com>
2014-07-04 10:05:50 +0200

    Added the show() and edit() functions to shell.py

--- woost/scripts/shell.py
+++ woost/scripts/shell.py
@@ -3,6 +3,7 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+import webbrowser
 from cocktail.translations import *
 from cocktail.persistence import *
 from woost import app
@@ -16,4 +17,31 @@ def setup_shell(env):
     set_current_user(User.require_instance(qname = "woost.anonymous_user"))
     set_current_website(config.websites[0])
     load_extensions()
+
+def show(target):
+
+    if isinstance(target, Publishable):
+        webbrowser.open(target.get_uri(host = "!"))
+    elif hasattr(target, "__iter__"):
+        for item in target:
+            show(item)
+    else:
+        print "Can't show non-publishable object %r" % target
+
+def edit(target):
+
+    backoffice = Publishable.require_instance(qname = "woost.backoffice")
+
+    if isinstance(target, Item):
+        webbrowser.open(
+            backoffice.get_uri(
+                host = "!",
+                path = ["content", str(target.id)]
+            )
+        )
+    elif hasattr(target, "__iter__"):
+        for item in target:
+            edit(item)
+    else:
+        print "Can't edit object %r" % target
 
