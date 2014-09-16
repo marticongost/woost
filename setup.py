3612984b6f8f7f1c0412910ae5b7e5009c105ad7
Marti Congost <marti.congost@whads.com>
2014-09-16 15:29:56 +0200

    Updated the version attribute in the setup.py script to 'diablo', ditching the old style number based versioning scheme.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os.path import join, isdir
 
 setup(
     name = "woost",
-    version = "0.9",
+    version = "diablo",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     maintainer = "Marti Congost",
@@ -53,7 +53,7 @@ of its features include:\n"
     ],
     install_requires = [
         "simplejson",
-        "cocktail==0.6"
+        "cocktail==baileys"
     ],    
     extras_require = {
         'campaign_monitor_api': ["campaign_monitor_api"],
