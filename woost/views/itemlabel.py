0bf895d6273fb312cc6a2077af618f2963c2e0a7
Marti Congost <marti.congost@whads.com>
2015-03-03 17:20:42 +0100

    Automatically hide irrelevant icons in the backoffice.
    The icon_visible property of ItemLabel / ItemDisplay now defaults to a new
    value, "auto". When set to "auto", these views will only display icons that the
    backoffice deems relevant to the current view.
    
    Models can force their icons to be displayed by setting the
    Item.icon_conveys_information property to True.

--- woost/views/itemlabel.py
+++ woost/views/itemlabel.py
@@ -6,16 +6,18 @@ u"""
 @organization:	Whads/Accent SL
 @since:			November 2008
 """
+from cocktail import schema
 from cocktail.html import Element
 from cocktail.translations import translations
 from cocktail.controllers import context
+from woost.models.rendering import Renderer
 
 
 class ItemLabel(Element):
 
     item = None
     image_factory = "backoffice_small_thumbnail.png"
-    icon_visible = True
+    icon_visible = "auto"
     thumbnail = True
     referer = None
     language_chain = None
@@ -29,6 +31,45 @@ class ItemLabel(Element):
 
     value = property(_get_value, _set_value)
 
+    def should_display_icon(self):
+
+        if self.icon_visible == "auto":
+
+            # Models can request that their icons be always visible
+            if self.item.icon_conveys_information:
+                return True
+
+            # Distinguish types
+            if self.member:
+                count = 0
+
+                if isinstance(self.member, schema.Reference):
+                    related_type = self.member.related_type
+                elif isinstance(self.member, schema.Schema):
+                    related_type = self.member
+                else:
+                    related_type = None
+
+                if related_type is not None:
+                    for cls in related_type.schema_tree():
+                        if cls.icon_conveys_information:
+                            return True
+                        if cls.visible and cls.instantiable:
+                            count += 1
+                            if count > 1:
+                                return True
+
+            # Images
+            renderer = Renderer.get_instance(qname = "woost.content_renderer")
+            if renderer is not None:
+                image = self.item.get_representative_image()
+                if image is not None and renderer.can_render(image):
+                    return True
+
+            return False
+
+        return self.icon_visible
+
     def _ready(self):
         Element._ready(self)
 
@@ -39,7 +80,7 @@ class ItemLabel(Element):
             for schema in self.item.__class__.descend_inheritance(True):
                 self.add_class(schema.name)
 
-            if self.icon_visible:
+            if self.should_display_icon():
                 self.add_class("with_icon")
                 self.append(self.create_icon())
 
