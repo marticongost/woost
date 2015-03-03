23f4a35ad2070ae74517af83c0ee860ef799f869
Marti Congost <marti.congost@whads.com>
2015-03-03 12:42:51 +0100

    Make the dropdown card in ItemDisplay act as a context menu.
    Previously, the card was displayed when clicking the icon; changed to drop down
    as a replacement of the label's context menu.

--- woost/views/resources/scripts/ItemDisplay.js
+++ woost/views/resources/scripts/ItemDisplay.js
@@ -9,6 +9,7 @@
 
 (function () {
     var PREFIX = "data-woost-ItemDisplay-";
+    var MIN_CONNECTOR_POSITION = 12;
 
     var ALIGNMENTS = [
         ["bottom", "right"],
@@ -19,28 +20,23 @@
 
     cocktail.bind(".ItemDisplay", function ($display) {
 
-        if (!this.connectorMargin) {
-            this.connectorMargin = 10;
-        }
-
         var $label = $display.find(".item_label");
         var itemId = $label.attr("data-woost-item");
         var $panel = null;
         var $connector = null;
-
-        var $icon = $label.find(".icon");
-        var $panelHandle = jQuery("<button type='button'>")
-            .addClass("panel_handle")
-            .insertBefore($icon)
-            .append($icon);
+        var currentConnectorPosition = undefined;
 
         this.getExpanded = function () {
             return $display.attr("data-woost-ItemDisplay-expanded") == "true";
         }
 
-        this.setExpanded = function (expanded) {
+        this.setExpanded = function (expanded, connectorPosition /* undefined */) {
             if (expanded) {
                 closeExpandedDisplay();
+                if (connectorPosition !== undefined) {
+                    connectorPosition = Math.max(connectorPosition, MIN_CONNECTOR_POSITION);
+                }
+                currentConnectorPosition = connectorPosition;
 
                 // Create the panel for the item
                 if (!$panel) {
@@ -86,23 +82,29 @@
                 $display.attr(PREFIX + "valign", valign);
                 $display.attr(PREFIX + "halign", halign);
 
-                var hcenter = $panelHandle.outerWidth() / 2;
-                $connector.css("left", hcenter - $connector.outerWidth() / 2 + "px");
+                $connector.css("left",
+                    (currentConnectorPosition === undefined ?
+                        $label.outerWidth() / 2
+                        : currentConnectorPosition
+                    )
+                    - $connector.outerWidth() / 2
+                    + "px"
+                );
 
                 if (halign == "right") {
-                    $panel.css("left", hcenter - $connector.outerWidth() / 2 - $display[0].connectorMargin + "px");
+                    $panel.css("left", 0);
                 }
                 else if (halign == "left") {
-                    $panel.css("left", hcenter - $panel.outerWidth() + $connector.outerWidth() / 2 + $display[0].connectorMargin + "px");
+                    $panel.css("left", $panel.outerWidth() + "px");
                 }
 
                 if (valign == "bottom") {
-                    $connector.css("top", $panelHandle.outerHeight() + "px");
-                    $panel.css("top", $panelHandle.outerHeight() + $connector.outerHeight() - 1 + "px");
+                    $connector.css("top", $label.outerHeight() + "px");
+                    $panel.css("top", $label.outerHeight() + $connector.outerHeight() - 1 + "px");
                 }
                 else if (valign == "top") {
                     $connector.css("top", -$connector.outerHeight() + "px");
-                    $panel.css("top", -$connector.outerHeight() - $panel.outerHeight() + "px");
+                    $panel.css("top", -$connector.outerHeight() - $panel.outerHeight() + 1 + "px");
                 }
 
                 // Make sure the panel fits the viewport; otherwise, try the next alignment
@@ -149,12 +151,13 @@
             ctx.restore();
         }
 
-        this.toggleExpanded = function () {
-            this.setExpanded(!this.getExpanded());
+        this.toggleExpanded = function (connectorPosition /* optional */) {
+            this.setExpanded(!this.getExpanded(), connectorPosition);
         }
 
-        $panelHandle.click(function (e) {
-            $display.get(0).toggleExpanded();
+        $label.on("contextmenu", function (e) {
+            var clickPosition = e.pageX - $label.offset().left;
+            $display.get(0).toggleExpanded(clickPosition);
             return false;
         });
 
@@ -183,7 +186,7 @@
         jQuery(document).click(closeExpandedDisplay);
         jQuery(window).resize(function () {
             jQuery(".ItemDisplay[" + PREFIX + "expanded='true']").each(function () {
-                this.alignPanel(false);
+                this.alignPanel();
             });
         });
     });
