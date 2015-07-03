622ec2cfa2b5012fb0d8054912b525f1c2bdda6c
Marti Congost <marti.congost@whads.com>
2015-07-03 13:59:30 +0200

    Use unordered sets for Website.specific_content and Publishable.websites

--- woost/models/publishable.py
+++ woost/models/publishable.py
@@ -204,6 +204,7 @@ class Publishable(Item):
 
     websites = schema.Collection(
         items = "woost.models.Website",
+        default_type = set,
         bidirectional = True,
         related_key = "specific_content",
         edit_control = "cocktail.html.CheckList",
