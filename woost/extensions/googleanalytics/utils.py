dc584d2608b5729c12fc3ce4c200b31983eb2fe2
Marti Congost <marti.congost@whads.com>
2015-11-03 13:20:50 +0100

    googleanalytics: Added the utils.fix_identifier() function

--- woost/extensions/googleanalytics/utils.py
+++ woost/extensions/googleanalytics/utils.py
@@ -3,7 +3,9 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+import re
 from json import dumps
+from cocktail.stringutils import normalize, html_to_plain_text
 from woost.models import Configuration
 
 def add_event(
@@ -30,4 +32,15 @@ def add_event(
         "woost.ga.triggerEventOnClick(this, %s);"
         % dumps(event_data)
     )
+
+_fix_identifier_nonword_expr = re.compile("\W", re.UNICODE)
+_fix_identifier_repeat_expr = re.compile("--+")
+
+def fix_identifier(id):
+    if id:
+        id = html_to_plain_text(id)
+        id = normalize(id)
+        id = _fix_identifier_nonword_expr.sub("-", id).strip("-")
+        id = _fix_identifier_repeat_expr.sub("-", id)
+    return id
 
