96228bdfc444927a76261a65da8286f6c7c90904
Marti Congost <marti.congost@whads.com>
2015-11-05 17:42:22 +0100

    Preserve query string parameters when performing URI and Document redirections.

--- woost/controllers/uricontroller.py
+++ woost/controllers/uricontroller.py
@@ -6,14 +6,20 @@ u"""
 @organization:	Whads/Accent SL
 @since:			March 2009
 """
-
 import cherrypy
+from cocktail.controllers import get_state
 from woost.controllers import BaseCMSController
 
 
 class URIController(BaseCMSController):
 
     def __call__(self, *args, **kwargs):
-        uri = self.context["publishable"].get_uri()
+
+        if self.is_internal_content():
+            parameters = get_state()
+        else:
+            parameters = None
+
+        uri = self.context["publishable"].get_uri(parameters = parameters)
         raise cherrypy.HTTPRedirect(uri)
 
