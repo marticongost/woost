522dadb7e57179d2c1a653f7989331515364cedc
Marti Congost <marti.congost@whads.com>
2015-11-05 17:41:29 +0100

    Added the Publishable.is_internal_content() method

--- woost/extensions/vimeo/vimeovideo.py
+++ woost/extensions/vimeo/vimeovideo.py
@@ -55,6 +55,9 @@ class VimeoVideo(Publishable):
         encode = True):
         return self.uri_pattern % (self.video_id,)
 
+    def is_internal_content(self, language = None):
+        return False
+
 
 def import_url_as_vimeo_video(url_import):
 
