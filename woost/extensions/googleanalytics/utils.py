5d1dafc23a1139a2ca7bfb4fb26b2afea8ed1fc1
Marti Congost <marti.congost@whads.com>
2015-11-10 13:27:16 +0100

    googleanalytics: renamed and changed the behavior of fix_identifier and get_object_identifier.
    fix_identifier() is renamed to escape_ga_string(), while get_object_identifier
    becomes get_ga_value(), to reflect the fact it can now deal with arbitrary
    values, not just persistent objects and types.

--- woost/extensions/googleanalytics/utils.py
+++ woost/extensions/googleanalytics/utils.py
@@ -5,7 +5,12 @@ u"""
 """
 import re
 from json import dumps
+from decimal import Decimal
+from collections import Iterable
+from cocktail.modeling import ListWrapper, SetWrapper
 from cocktail.stringutils import normalize, html_to_plain_text
+from cocktail.translations import translations
+from cocktail.persistence import PersistentObject
 from woost.models import Configuration
 
 def add_event(
@@ -33,14 +38,54 @@ def add_event(
         % dumps(event_data)
     )
 
-_fix_identifier_nonword_expr = re.compile("\W", re.UNICODE)
-_fix_identifier_repeat_expr = re.compile("--+")
+_escape_ga_string_nonword_expr = re.compile("\W", re.UNICODE)
+_escape_ga_string_repeat_expr = re.compile("--+")
 
-def fix_identifier(id):
+def escape_ga_string(id):
     if id:
         id = html_to_plain_text(id)
         id = normalize(id)
-        id = _fix_identifier_nonword_expr.sub("-", id).strip("-")
-        id = _fix_identifier_repeat_expr.sub("-", id)
+        id = _escape_ga_string_nonword_expr.sub("-", id).strip("-")
+        id = _escape_ga_string_repeat_expr.sub("-", id)
     return id
+
+def get_ga_value(value, language = None):
+
+    if language is None:
+        language = Configuration.instance.google_analytics_language
+
+    if value is None:
+        return ""
+    elif isinstance(value, basestring):
+        return escape_ga_string(value)
+    elif isinstance(value, bool):
+        return translations(value, language = language)
+    elif isinstance(value, (int, float, Decimal)):
+        return str(value)
+    elif isinstance(value, type):
+        return escape_ga_string(translations(value.__name__, language = language))
+    elif isinstance(value, (set, frozenset, SetWrapper)):
+        value = map(get_ga_value, value)
+        value.sort()
+        return " ".join(value)
+    elif isinstance(value, Iterable):
+        return " ".join(get_ga_value(item) for item in value)
+    elif isinstance(value, PersistentObject):
+        return (
+            u"%s --%d--" % (
+                escape_ga_string(
+                    translations(
+                        value,
+                        language = language,
+                        discard_generic_translation = True
+                    )
+                ),
+                value.id
+            )
+        ).strip()
+    else:
+        raise ValueError(
+            "%r is not a valid value for get_ga_value()"
+            % value
+        )
 
