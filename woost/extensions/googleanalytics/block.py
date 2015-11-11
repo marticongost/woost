4e25d578a9a829335bf65e3661b02d3137f62722
Marti Congost <marti.congost@whads.com>
2015-11-11 13:50:34 +0100

    googleanalytics: automatically generate events for links and listings.
    The changeset moves all the members for Google Analytics event customization
    that had been added to TextBlock to Block. Also, cocktail.html.Element now
    offers several new properties to hold and propagate that information.
    
    The changeset adds overlays for the woost.views.Link, woost.views.NewsListing
    and woost.views.EventListing views, to automatically trigger events based on the
    aforementioned properties.

---
+++ woost/extensions/googleanalytics/block.py
@@ -0,0 +1,66 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from cocktail import schema
+from woost.models import Block
+from .utils import escape_ga_string, get_ga_value
+
+Block.members_order += [
+    "generates_ga_events",
+    "ga_event_category",
+    "ga_event_action",
+    "ga_event_label"
+]
+
+Block.add_member(
+    schema.Boolean("generates_ga_events",
+        default = True,
+        member_group = "google_analytics"
+    )
+)
+
+Block.add_member(
+    schema.String("ga_event_category",
+        normalization = escape_ga_string,
+        member_group = "google_analytics"
+    )
+)
+
+Block.add_member(
+    schema.String("ga_event_action",
+        normalization = escape_ga_string,
+        member_group = "google_analytics"
+    )
+)
+
+Block.add_member(
+    schema.String("ga_event_label",
+        normalization = escape_ga_string,
+        member_group = "google_analytics"
+    )
+)
+
+base_init_view = Block.init_view
+
+def init_view(self, view):
+
+    view.generates_ga_events = self.generates_ga_events
+
+    view.ga_event_category = (
+        self.ga_event_category
+        or get_ga_value(self.__class__)
+    )
+
+    view.ga_event_action = self.ga_event_action or "click"
+
+    view.ga_event_label = (
+        self.ga_event_label
+        or get_ga_value(self)
+    )
+
+    base_init_view(self, view)
+
+Block.init_view = init_view
+
