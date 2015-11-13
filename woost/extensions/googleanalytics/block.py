e6d545cf642856fa7773d79eec4329fa6b9d5f02
Marti Congost <marti.congost@whads.com>
2015-11-13 13:57:15 +0100

    googleanalytics: removed all the event customization members in Block.
    It seems doubtful that anyone would want to change the default naming
    convention. Also, the interactions between those fields, the default naming
    convention for blocks and the overrides applied by block views could be
    difficult to explain.

--- woost/extensions/googleanalytics/block.py
+++ woost/extensions/googleanalytics/block.py
@@ -3,52 +3,21 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from cocktail import schema
 from woost.models import Block
 from .utils import escape_ga_string, get_ga_value
-
-Block.members_order += [
-    "ga_event_category",
-    "ga_event_action",
-    "ga_event_label"
-]
-
-Block.add_member(
-    schema.String("ga_event_category",
-        normalization = escape_ga_string,
-        member_group = "google_analytics"
-    )
-)
-
-Block.add_member(
-    schema.String("ga_event_action",
-        normalization = escape_ga_string,
-        member_group = "google_analytics"
-    )
-)
-
-Block.add_member(
-    schema.String("ga_event_label",
-        normalization = escape_ga_string,
-        member_group = "google_analytics"
-    )
-)
 
 base_init_view = Block.init_view
 
 def init_view(self, view):
 
-    view.ga_event_category = (
-        self.ga_event_category
-        or get_ga_value(self.__class__)
-    )
+    if not view.ga_event_category:
+        view.ga_event_category = get_ga_value(self.__class__)
 
-    view.ga_event_action = self.ga_event_action or "click"
+    if not view.ga_event_action:
+        view.ga_event_action = "click"
 
-    view.ga_event_label = (
-        self.ga_event_label
-        or get_ga_value(self)
-    )
+    if not view.ga_event_label:
+        view.ga_event_label = get_ga_value(self)
 
     base_init_view(self, view)
 
