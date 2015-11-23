3d19145a16ece2752f0bad5f6940c496dc8c121d
Marti Congost <marti.congost@whads.com>
2015-11-23 13:22:54 +0100

    googleanalytics: use the 'beacon' transport to send events.
    The event injection script caused conflicts with other scripts (f. eg.
    fancybox). By setting the "transport" option to "beacon", the injector doesn't
    need to suppress and recreate click events.
    
    The change also polyfills navigator.sendBeacon()

--- woost/views/resources/scripts/googleanalytics.js
+++ woost/views/resources/scripts/googleanalytics.js
@@ -17,6 +17,7 @@ woost.ga.activateWithEvent = function (element, eventData) {
     for (var key in eventData) {
         options[key] = eventData[key];
     }
+    options.transport = "beacon";
 
     if (element.tagName == "A") {
         options.hitCallback = function () {
@@ -99,14 +100,35 @@ woost.ga.getEventData = function (element) {
 
 woost.ga.eventTrigger = function (e) {
     var gaEvent = woost.ga.getEventData(this);
-    if (gaEvent.eventCategory && gaEvent.eventAction) {
-        woost.ga.activateWithEvent(this, gaEvent);
-        e.preventDefault();
-    }
+    gaEvent.transport = "beacon";
+    ga("send", "event", gaEvent);
 }
 
 jQuery(function () {
     jQuery(document).on("click", "a, button, input[type='submit']", woost.ga.eventTrigger);
     jQuery(document).on("submit", "form", woost.ga.eventTrigger);
 });
+
+// navigator.sendBeacon polyfill (copied from https://github.com/miguelmota/Navigator.sendBeacon)
+(function(root) {
+  'use strict';
+
+  if (!('sendBeacon' in navigator)) {
+    navigator.sendBeacon = function(url, data) {
+      var xhr = ('XMLHttpRequest' in root) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+      xhr.open('POST', url, false);
+      xhr.setRequestHeader('Accept', '*/*');
+      if (typeof data === 'string') {
+        xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+        xhr.responseType = 'text/plain';
+      } else if (Object.prototype.toString.call(data) === '[object Blob]') {
+        if (data.type) {
+          xhr.setRequestHeader('Content-Type', data.type);
+        }
+      }
+      xhr.send(data);
+      return true;
+    };
+  }
+})(this);
 
