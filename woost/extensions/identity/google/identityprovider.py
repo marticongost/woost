c31064b535678564493b2c3b00467306498e6ace
Marti Congost <marti.congost@whads.com>
2015-12-15 09:55:27 +0100

    identity: fixed the refresh token flow for the Google provider

--- woost/extensions/identity/google/identityprovider.py
+++ woost/extensions/identity/google/identityprovider.py
@@ -6,8 +6,11 @@ u"""
 import json
 import urllib
 import urllib2
+from cocktail.styled import styled
 from cocktail import schema
 from cocktail.controllers import Location
+from woost import app
+from woost.models import Configuration
 from woost.extensions.identity.identityprovider import IdentityProvider
 
 
@@ -64,14 +67,21 @@ class GoogleIdentityProvider(IdentityProvider):
         authorization code.
         """
         OAUTH_URL = "https://accounts.google.com/o/oauth2/auth"
-        REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+
+        if not redirect_uri:
+            website = (app.website or Configuration.instance.websites[0])
+            redirect_uri = "http://%s/google_oauth/%d/refresh_token" % (
+                website.hosts[0],
+                self.id
+            )
 
         params = {
             "client_id": self.client_id,
-            "redirect_uri": redirect_uri or REDIRECT_URI,
+            "redirect_uri": redirect_uri,
             "scope": " ".join(self.scope),
             "response_type": "code",
-            "access_type": self.access_type
+            "access_type": self.access_type,
+            "approval_prompt": "force"
         }
 
         import webbrowser
@@ -88,19 +98,28 @@ class GoogleIdentityProvider(IdentityProvider):
         key can be used to generate new valid access tokens
         """
         TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
-        REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+
+        if not redirect_uri:
+            website = (app.website or Configuration.instance.websites[0])
+            redirect_uri = "http://%s/google_oauth/%d/refresh_token" % (
+                website.hosts[0],
+                self.id
+            )
 
         params = {
             "code": code,
             "client_id": self.client_id,
             "client_secret": self.client_secret,
-            "redirect_uri": redirect_uri or REDIRECT_URI,
-            "grant_type": "authorization_code",
+            "redirect_uri": redirect_uri,
+            "grant_type": "authorization_code"
         }
 
         request = urllib2.Request(url=TOKEN_URL, data=urllib.urlencode(params))
         json_file = urllib2.urlopen(request).read()
         auth_token = json.loads(json_file)
+
+        if self.debug_mode:
+            print styled("Google refresh token:", "magenta"), auth_token
 
         return auth_token
 
@@ -117,12 +136,16 @@ class GoogleIdentityProvider(IdentityProvider):
             "client_id": self.client_id,
             "client_secret": self.client_secret,
             "refresh_token": refresh_token,
-            "grant_type": "refresh_token",
+            "grant_type": "refresh_token"
         }
 
         request = urllib2.Request(url=TOKEN_URL, data=urllib.urlencode(params))
         json_file = urllib2.urlopen(request).read()
         access_token = json.loads(json_file)
+
+        if self.debug_mode:
+            print styled("Google access token from refresh token:", "magenta"),
+            print access_token
 
         return access_token
 
