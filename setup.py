9507cdcf03b543ae9dcba7dd965f9724a7cb2ae6
Marti Congost <marti.congost@whads.com>
2016-02-15 17:41:52 +0100

    Fixed the dependency specifier for cocktail

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ of its features include:\n"
     ],
     install_requires = [
         "simplejson",
-        "cocktail==1.5"
+        "cocktail>=1.5,<1.6"
     ],
     extras_require = {
         'campaign_monitor_api': ["campaign_monitor_api"],
