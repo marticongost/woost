ed0c22e7034476f3c36472fccc25ea4043733b3e
Marti Congost <marti.congost@whads.com>
2016-02-15 17:46:34 +0100

    New version: 2.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os.path import join, isdir
 
 setup(
     name = "woost",
-    version = "2.2",
+    version = "2.2.1",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     maintainer = "Marti Congost",
