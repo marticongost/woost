462827f955c860372fec13dbc559f343a83a435d
Marti Congost <marti.congost@whads.com>
2016-03-01 17:48:45 +0100

    Added support for custom keyboard shortcuts in the rich text editor.
    The new Style.editor_keyboard_shortcut makes it easy to bind custom styles to
    user defined keyboard shortcuts.
    
    In order to implement this, a 'shortcuts' property was added to the
    RichTextEditor view.

--- woost/views/richtexteditor.py
+++ woost/views/richtexteditor.py
@@ -7,6 +7,7 @@ u"""
 @since:			October 2008
 """
 from cocktail.html import templates
+from cocktail.html.utils import is_inline_element
 from cocktail.translations import translations
 from woost.models import Style
 
@@ -16,6 +17,8 @@ TinyMCE = templates.get_class("cocktail.html.TinyMCE")
 class RichTextEditor(TinyMCE):
 
     styles = None
+    shortcuts = None
+
     tinymce_params = {
         "setup": "woost.richTextEditor.init",
         "plugins": "table link image fullscreen code "
@@ -59,6 +62,10 @@ class RichTextEditor(TinyMCE):
                    "bullist numlist outdent indent"
     }
 
+    def __init__(self, *args, **kwargs):
+        TinyMCE.__init__(self, *args, **kwargs)
+        self.shortcuts = list(self.shortcuts) if self.shortcuts else []
+
     def _build(self):
         TinyMCE._build(self)
         self.add_resource("/resources/scripts/RichTextEditor.js")
@@ -67,11 +74,24 @@ class RichTextEditor(TinyMCE):
         return list(Style.select({"applicable_to_text": True}))
 
     def _get_tinymce_style_definition(self, style):
-        return {
-            "title": translations(style),
+        style_dfn = self._get_tinymce_format_definition(style)
+        style_dfn["title"] = translations(style)
+        return style_dfn
+
+    def _get_tinymce_format_definition(self, style):
+
+        format_dfn = {
             "inline": "span",
             "classes": style.class_name
         }
+
+        if style.html_tag:
+            if is_inline_element(style.html_tag):
+                format_dfn["inline"] = style.html_tag
+            else:
+                format_dfn["block"] = style.html_tag
+
+        return format_dfn
 
     def _ready(self):
 
@@ -89,6 +109,26 @@ class RichTextEditor(TinyMCE):
                     for style in styles
                 ]
             }])
+
+            shortcuts = list(self.shortcuts)
+            formats = {}
+
+            for style in styles:
+                if style.editor_keyboard_shortcut:
+                    format_id = style.class_name
+                    formats[format_id] = \
+                        self._get_tinymce_format_definition(style)
+                    shortcuts.append([
+                        style.editor_keyboard_shortcut,
+                        translations(style),
+                        "this.formatter.apply('%s');" % format_id
+                    ])
+
+            if formats:
+                self.tinymce_params.setdefault("formats", formats)
+
+            if shortcuts:
+                self.tinymce_params["woostShortcuts"] = shortcuts
 
         TinyMCE._ready(self)
 
