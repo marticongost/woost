cc360b3baced51c1b9504b70303c54ba2856b394
Eduard Nadal <eduard.nadal@whads.com>
2016-04-15 14:57:33 +0200

    Added the Google Tag Manager extension

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ of its features include:\n"
 			"signup = woost.extensions.signup:SignUpExtension",
             "googlesearch = woost.extensions.googlesearch:GoogleSearchExtension",
             "googleanalytics = woost.extensions.googleanalytics:GoogleAnalyticsExtension",
+            "googletagmanager=woost.extensions.googletagmanager:GoogleTagManagerExtension",
             "campaignmonitor = woost.extensions.campaignmonitor:CampaignMonitorExtension",
             "mailer = woost.extensions.mailer:MailerExtension",
             "usermodels = woost.extensions.usermodels:UserModelsExtension",
