cefa02d44bf52081da0f94e65ac98188fadabeb1
V?ctor Manuel Ag?ero <V?ctor Manuel Ag?ero@localhost>
2017-01-20 13:35:52 +0100

    New NoCaptcha Extension

---
+++ woost/extensions/nocaptcha/website.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Víctor Manuel Agüero Requena <victor.aguero@whads.com>
+"""
+from cocktail import schema
+from woost.models import Website
+
+Website.add_member(
+    schema.String(
+        "nocaptcha_public_key",
+        required = True,
+        member_group = "services.nocaptcha",
+        text_search = False,
+        listed_by_default = False
+    )
+)
+
+Website.add_member(
+    schema.String(
+        "nocaptcha_private_key",
+        required = True,
+        member_group = "services.nocaptcha",
+        text_search = False,
+        listed_by_default = False
+    )
+)
+
