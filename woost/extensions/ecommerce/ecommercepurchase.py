a17f8c0c697a837755f7655d9638ea5182525164
Marti Congost <marti.congost@whads.com>
2017-03-13 16:20:20 +0100

    ecommerce: fixes to translations after cocktail lambanog

--- woost/extensions/ecommerce/ecommercepurchase.py
+++ woost/extensions/ecommerce/ecommercepurchase.py
@@ -207,11 +207,14 @@ class ECommercePurchase(Item):
                 yield member
 
 @translations.instances_of(ECommercePurchase)
-def translate_ecommerce_purchase(self, language, **kwargs):
+def translate_ecommerce_purchase(self, **kwargs):
+
+    if not self.quantity or not self.product:
+        return None
 
     desc = u"%d x %s" % (
         self.quantity,
-        translations(self.product, language)
+        translations(self.product)
     )
 
     options = []
@@ -219,8 +222,8 @@ def translate_ecommerce_purchase(self, language, **kwargs):
         if member is ECommercePurchase.quantity:
             continue
         options.append("%s: %s" % (
-            translations(member, language),
-            member.translate_value(self.get(member), language)
+            translations(member),
+            member.translate_value(self.get(member))
         ))
 
     if options:
