2a257dffdb9beac7efae056795406dc9dddaeb68
Marti Congost <marti.congost@whads.com>
2018-02-05 16:58:03 +0100

    Added the @with_default_template class decorator

--- woost/models/__init__.py
+++ woost/models/__init__.py
@@ -59,6 +59,7 @@ from .publishable import (
     user_has_access_level
 )
 from .document import Document
+from .defaulttemplate import with_default_template
 from .theme import Theme
 from .template import Template
 from .controller import Controller
--- woost/models/website.py
+++ woost/models/website.py
@@ -72,9 +72,6 @@ class Website(Item):
 
         # presentation
         "theme",
-        "default_page_template",
-        "default_news_template",
-        "default_event_template",
 
         # meta
         "meta_tags",
@@ -267,24 +264,6 @@ class Website(Item):
         related_end = schema.Collection(),
         listed_by_default = False,
         member_group = "publication"
-    )
-
-    default_page_template = schema.Reference(
-        type = Template,
-        related_end = schema.Collection(),
-        member_group = "presentation.appearence"
-    )
-
-    default_news_template = schema.Reference(
-        type = Template,
-        related_end = schema.Collection(),
-        member_group = "presentation.appearence"
-    )
-
-    default_event_template = schema.Reference(
-        type = Template,
-        related_end = schema.Collection(),
-        member_group = "presentation.appearence"
     )
 
     # meta
