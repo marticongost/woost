26a501c802ba4c1cc5cd504c57ebb348725eb4b4
Marti Congost <marti.congost@whads.com>
2018-04-23 13:49:09 +0200

    Added the Grid.side_margin member

--- woost/models/grid.py
+++ woost/models/grid.py
@@ -14,6 +14,7 @@ class Grid(Item):
     members_order = [
         "title",
         "column_count",
+        "side_margin",
         "sizes"
     ]
 
@@ -28,6 +29,11 @@ class Grid(Item):
     column_count = schema.Integer(
         required = True,
         default = 12
+    )
+
+    side_margin = schema.Integer(
+        required = True,
+        default = 50
     )
 
     sizes = schema.Collection(
@@ -61,9 +67,10 @@ class Grid(Item):
                 )
             )
 
-        return "@include define_grid((%s), $col-count: %d);" % (
+        return "@include define_grid((%s), $col-count: %d, $margin: %d);" % (
             ", ".join(size_defs),
-            self.column_count
+            self.column_count,
+            self.side_margin
         )
 
 
