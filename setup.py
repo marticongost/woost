b211865377ffa26c6777cb8090a3c1d1e55419ec
Marti Congost <marti.congost@whads.com>
2018-05-23 15:00:36 +0200

    Code formatting fixes

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ of its features include:\n"
             "sitemap = woost.extensions.sitemap:SitemapExtension",
             "pdf = woost.extensions.pdf:PDFExtension",
             "vimeo = woost.extensions.vimeo:VimeoExtension",
-			"signup = woost.extensions.signup:SignUpExtension",
+            "signup = woost.extensions.signup:SignUpExtension",
             "googlesearch = woost.extensions.googlesearch:GoogleSearchExtension",
             "googleanalytics = woost.extensions.googleanalytics:GoogleAnalyticsExtension",
             "googletagmanager=woost.extensions.googletagmanager:GoogleTagManagerExtension",
@@ -111,7 +111,7 @@ of its features include:\n"
             "identity = woost.extensions.identity:IdentityExtension",
             "attributes = woost.extensions.attributes:AttributesExtension",
             "staticpub = woost.extensions.staticpub:StaticPubExtension",
-			"nocaptcha = woost.extensions.nocaptcha:NoCaptchaExtension"
+            "nocaptcha = woost.extensions.nocaptcha:NoCaptchaExtension"
         ]
     },
     # Woost can't yet access view resources (images, style sheets, client
