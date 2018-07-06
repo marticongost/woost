3fd41e7bc8f5b67c7bcf75a80d80c64aaeebfef8
Marti Congost <marti.congost@whads.com>
2018-07-06 09:23:51 +0200

    googleanalytics: generate events for clicks on menus

--- woost/extensions/googleanalytics/__init__.py
+++ woost/extensions/googleanalytics/__init__.py
@@ -55,6 +55,8 @@ class GoogleAnalyticsExtension(Extension):
         from cocktail.html import templates
         templates.get_class("woost.extensions.googleanalytics.BaseViewOverlay")
         templates.get_class("woost.extensions.googleanalytics.LinkOverlay")
+        templates.get_class("woost.extensions.googleanalytics.MenuOverlay")
+        templates.get_class("woost.extensions.googleanalytics.StandardViewOverlay")
         templates.get_class("woost.extensions.googleanalytics.TextBlockViewOverlay")
         templates.get_class("woost.extensions.googleanalytics.LanguageSelectorOverlay")
 
