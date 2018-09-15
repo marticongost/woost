3cec49e37fc6340ee709be723a89f0fea6e8884f
Marti Congost <marti.congost@whads.com>
2018-09-15 19:46:17 +0200

    Added an admin section to set default controllers

---
+++ woost/admin/sections/defaultcontrollerssection.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from woost.models import get_default_controllers
+from .settings import Settings
+
+
+class DefaultControllersSection(Settings):
+
+    icon_uri = None
+
+    @property
+    def members(self):
+        return [member.name for member in get_default_controllers()]
+
