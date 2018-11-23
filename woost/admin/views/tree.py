5f1884f9917ad18b566bc77228c8b91750250037
Marti Congost <marti.congost@whads.com>
2018-11-23 13:23:57 +0100

    Simplify the definition of tree views in the admin
    - Moved View.children_collection to the Tree class
    - Generate a default TreeExport subclass based on the given children_collection

--- woost/admin/views/tree.py
+++ woost/admin/views/tree.py
@@ -3,8 +3,48 @@ u"""
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from .views import View
+from cocktail.modeling import overrides
+from woost.admin.dataexport import TreeExport
+from .views import View, DEFAULT
+
 
 class Tree(View):
-    pass
+    """A view for hierarchical data."""
+
+    children_collection = None
+
+    def __init__(self, name, children_collection, **kwargs):
+        """Initializes the tree view.
+
+        Accepts the same parameters as `View.__init__`, with the following
+        additions:
+
+        :param children_collection: The collection that the tree should be
+            based on (example: Document.children).
+        :type children_collection: `~cocktail.schema.Collection`
+        """
+        # Default data exporter
+        if kwargs.get("export_class", DEFAULT) is DEFAULT:
+            kwargs["export_class"] = \
+                self._get_default_tree_export(children_collection)
+
+        View.__init__(self, name, **kwargs)
+        self.__children_collection = children_collection
+
+    def _get_default_tree_export(self, collection):
+
+        class DefaultTreeExport(TreeExport):
+            children_collection = collection
+
+        return DefaultTreeExport
+
+    @property
+    def children_collection(self):
+        return self.__children_collection
+
+    @overrides(View.export_data)
+    def export_data(self):
+        data = View.export_data(self)
+        data["children_collection"] = self.children_collection.name
+        return data
 
