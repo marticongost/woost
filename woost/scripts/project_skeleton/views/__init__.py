482f63d17be9b2e262da7d34c3cbaa1e9986f58d
Marti Congost <marti.congost@whads.com>
2018-11-27 11:43:42 +0100

    Removed a redundant resource repository declaration in the project skeleton (already done at controllers/__init__.py)

--- woost/scripts/project_skeleton/views/__init__.py
+++ woost/scripts/project_skeleton/views/__init__.py
@@ -2,13 +2,6 @@
 u"""
 Site specific views.
 """
-from pkg_resources import resource_filename
-from cocktail.html import renderers, resource_repositories
+from cocktail.html import renderers
 renderers.default_renderer = renderers.html5_renderer
-
-resource_repositories.define(
-    "--SETUP-PACKAGE--",
-    "/--SETUP-FLAT_WEBSITE_NAME--_resources",
-    resource_filename("--SETUP-PACKAGE--.views", "resources")
-)
 
