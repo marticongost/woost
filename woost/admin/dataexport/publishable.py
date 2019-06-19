6e304469728760620b1fabc492f4206ac8f3c7a1
Marti Congost <marti.congost@whads.com>
2019-06-19 13:23:29 +0200

    Make the 'Show published' admin action work with PublishableObject, not just Publishable

--- woost/admin/dataexport/publishable.py
+++ woost/admin/dataexport/publishable.py
@@ -3,10 +3,10 @@
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from woost.models import Publishable
+from woost.models import PublishableObject
 from .dataexport import Export
 
-@Export.fields_for(Publishable)
+@Export.fields_for(PublishableObject)
 def publishable_fields(exporter, model, ref = False):
     yield (lambda obj, path: ("_url", obj.get_uri()))
 
