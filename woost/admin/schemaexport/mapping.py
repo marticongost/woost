9bfa4e5633405f204376653033117bc4e11b4a71
Marti Congost <marti.congost@whads.com>
2019-06-19 12:39:23 +0200

    Export the 'keys' and 'values' properties of Mapping members in the admin

--- woost/admin/schemaexport/mapping.py
+++ woost/admin/schemaexport/mapping.py
@@ -5,5 +5,26 @@
 """
 from cocktail import schema
 
+from .schemaexport import (
+    MemberExport,
+    exports_member,
+    member_exporters
+)
+
 schema.Mapping.ui_member_class = "cocktail.schema.Mapping"
+
+
+@exports_member(schema.Mapping)
+class MappingExport(MemberExport):
+
+    def get_properties(self, member, nested):
+        yield from super().get_properties(member, nested)
+
+        if member.keys:
+            exporter = member_exporters[member.keys.__class__]()
+            yield "keys", exporter.get_declaration(member.keys, nested=True)
+
+        if member.values:
+            exporter = member_exporters[member.values.__class__]()
+            yield "values", exporter.get_declaration(member.values, nested=True)
 
