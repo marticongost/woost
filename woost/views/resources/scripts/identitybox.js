e76be234aad4eab360edbf3078e79f00e5979bbe
Marti Congost <marti.congost@whads.com>
2019-07-08 13:22:52 +0200

    Turn IdentityBox into a dropdown panel with an optional login form

--- woost/views/resources/scripts/identitybox.js
+++ woost/views/resources/scripts/identitybox.js
@@ -8,17 +8,23 @@
 -----------------------------------------------------------------------------*/
 
 cocktail.bind(".IdentityBox", function ($box) {
+
+    var $panel = $box.find(".panel");
+
     if (woost.user) {
         if (woost.user.anonymous) {
             jQuery(document.documentElement).attr("data-woost-auth-state", "anonymous");
             $box.attr("data-woost-auth-state", "anonymous");
+            if ($box.is(".allows_login")) {
+                jQuery(cocktail.instantiate("woost.views.IdentityBox.login_form")).appendTo($panel);
+            }
         }
         else {
             jQuery(document.documentElement).attr("data-woost-auth-state", "authenticated");
             $box.attr("data-woost-auth-state", "authenticated");
-            var $boxContent = jQuery(cocktail.instantiate("woost.views.IdentityBox.box"));
+            var $boxContent = jQuery(cocktail.instantiate("woost.views.IdentityBox.current_user_info"));
             $boxContent.find(".user_label").html(woost.user.label);
-            $box.append($boxContent);
+            $panel.append($boxContent);
         }
     }
 });
