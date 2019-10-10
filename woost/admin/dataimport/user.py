85a30524a1cf3e4200df2538d79ca7cadc12b211
Marti Congost <marti.congost@whads.com>
2019-10-10 15:05:37 +0200

    Coding style tweaks in the admin.dataimport package

--- woost/admin/dataimport/user.py
+++ woost/admin/dataimport/user.py
@@ -1,14 +1,24 @@
-#-*- coding: utf-8 -*-
 """
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+from cocktail import schema
+from cocktail.jsonutils import json_object
+
 from woost.models import User
-from .dataimport import import_object, should_import_member
+from .dataimport import (
+    import_object,
+    should_import_member,
+    Import
+)
 from .item import should_import_item_member
 
 @should_import_member.implementation_for(User)
-def should_import_user_member(self, imp, data, member):
+def should_import_user_member(
+        self: User,
+        imp: Import,
+        data: json_object,
+        member: schema.Member) -> bool:
 
     if not should_import_item_member(self, imp, data, member):
         return False
